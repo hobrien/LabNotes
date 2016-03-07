bc175526d89a93cc0c797afb5e17957b88a474db
Heath O'Brien <heath.obrien@gmail.com>
2016-03-07 13:51:20 +0000

    changed ownership of /usr/local for home brew

--- InstallSoftware.sh
+++ InstallSoftware.sh
@@ -2,10 +2,10 @@
 
 # this requires user input: /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
-#brew cask install firefox
-#brew cask install istat-menus # requires licence key eventually
+brew cask install firefox
+brew cask install istat-menus # requires licence key eventually
 brew cask install bbedit # requires licence key eventually
-#brew cask install lastpass
+brew cask install lastpass
 brew cask install google-chrome
 brew cask install papers # requires licence key eventually
 brew cask install bittorrent-sync
@@ -15,6 +15,26 @@ brew cask install r
 brew cask install vienna
 brew cask install dropbox
 brew cask install yojimbo # requires licence key eventually
-brew  cask install anaconda
+brew cask install anaconda
+
+# Set up bash profile
+echo "~/.bash_profile" > ~/.bashrc # this doesn't appear to be working because a command using xargs couldn't find an executable in ~/bin
+echo 'export PATH="~/bin:/usr/local/sbin:$PATH"' > ~/.bash_profile
+echo 'source ~/anaconda3/bin/activate root' >> ~/.bash_profile
+
+# fix homebrew:
+sudo chown -R $(whoami) /usr/local/include
+sudo chown -R $(whoami) /usr/local/lib
+sudo chown -R $(whoami) /usr/local/lib/pkgconfig
+sudo chown -R $(whoami) /usr/local/share/man/man5
+sudo chown -R $(whoami) /usr/local/share/man/mann
+
+
+brew tap homebrew/science
+brew install samtools
 
 # install Mail ActOn
+
+pip install grip
+# this doesn't work no matter how I try to install it: pip install bx-python
+
