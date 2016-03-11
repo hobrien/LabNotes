09952a8cfa3062e9480cf1b1d772763a90f502bb
Heath O'Brien <heath.obrien@gmail.com>
2016-03-11 12:02:16 +0000

    final output copied to separate file
    This was necessary because there are WAY too many files to pick out the
    ones to upload to the imputation server. For some reason, the final
    gzip files were fubar. I think I ran these on my laptop and something
    clearly went wrong. When I reran it on my iMac everything is fine

--- Bash/FilterAndRecode.sh
+++ Bash/FilterAndRecode.sh
@@ -4,5 +4,5 @@ egrep 'Ambiguous|Ref not found' FB_Merged_chr$1.check.ref |cut -f 3 >FB_Merged_c
 cut -f 3 FB_Merged_chr$1.check.dup >>FB_Merged_chr$1_exclude.txt
 grep 'Strand switch' FB_Merged_chr$1.check.ref | cut -f 3 > FB_Merged_chr$1_flip.txt
 plink --bfile FB_Merged --chr $1 --exclude FB_Merged_chr$1_exclude.txt --flip FB_Merged_chr$1_flip.txt --recode vcf --out FB_Merged_chr$1_flip
-vcf-sort FB_Merged_chr$1_flip.vcf |bgzip -c > FB_Merged_chr$1_flip.vcf.gz
+vcf-sort FB_Merged_chr$1_flip.vcf |bgzip -c > Filtered/FB_Merged_chr$1_flip.vcf.gz
 ~/BTSync/Code/Python/checkVCFmod.py -r ~/Documents/src/checkVCF-20140116/hs37d5.fa -o FB_Merged_chr$1_flip FB_Merged_chr$1_flip.vcf
