2cf95929aece7859429296b40148c37ce9db3cf6
Heath O'Brien <heath.obrien@gmail.com>
2016-04-14 10:38:08 +0100

    Added sorting and indexing after mapping
    it would probably be better to just execute SubmissionScripts/Samtools
    using the bash command
    I also changed the locations of a number of the files

---
+++ SubmissionScripts/Tophat2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+# see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
+filename=${1##*/}
+sampleID=${filename%%_*}
+
+#mkdir /c8000xd3/rnaseq-heath/Mappings/$sampleID
+#tophat --keep-fasta-order --transcriptome-index /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.inx --library-type fr-secondstrand --mate-inner-dist 500  --mate-std-dev 50 --num-threads 8 --output-dir /c8000xd3/rnaseq-heath/Mappings/$sampleID /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/Bowtie2Index/genome $@
+#mkdir /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM
+#mv /c8000xd3/rnaseq-heath/Mappings/$sampleID/accepted_hits.bam /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/
+#mv /c8000xd3/rnaseq-heath/Mappings/$sampleID/unmapped.bam /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/
+#samtools sort /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/accepted_hits.bam /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.sort
+samtools index /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.sort.bam
