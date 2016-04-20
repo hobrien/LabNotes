1b1626b0d15086e3abecccfabb013a61de19f800
Heath O'Brien OBrienH1@cf.ac.uk <mpmho@rocks.psycm.cf.ac.uk>
2016-04-20 09:46:50 +0100

    added sorting and indexing

--- SubmissionScripts/Samtools.sh
+++ SubmissionScripts/Samtools.sh
@@ -6,7 +6,9 @@
 #
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
-#samtools index $@
-#samtools sort $@ ${@%.*}_sort.bam
 
-samtools mpileup -d 8000 -f /home/heath/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa -r chr1:98046571-98046571 $@
+folder_path=${@%/*}
+folder=${folder_path##*/}
+#samtools sort -o $folder_path/$folder.sort.bam $@
+samtools index $folder_path/$folder.sort.bam
+#samtools mpileup -d 8000 -f /home/mpmho/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa -r chr1:98046571-98046571 $@
