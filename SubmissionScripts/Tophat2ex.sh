bfb114f82a3f548971eba74ce3eb706e76b08fc8
Heath O'Brien <heath.obrien@gmail.com>
2016-04-21 12:52:50 +0100

    Added code to deal with Exeter sequences that are compressed
    Almost all of the Exeter data is uncompressed, but one sample is compressed
    
    This means that the trimmed reads for this sample are also compressed and they do not follow the naming convention of all the others

--- SubmissionScripts/Tophat2ex.sh
+++ SubmissionScripts/Tophat2ex.sh
@@ -9,17 +9,26 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 filename1=${1##*/}
-echo ${filename1%%.*} 
 filename2=${2##*/}
 sampleID=${filename1%%_*}
 
+if [ $sampleID == 16810 ]
+then
+    read1=/c8000xd3/rnaseq-heath/Trimmed/${filename1%%.*}_val_1.fq.gz
+    read2=/c8000xd3/rnaseq-heath/Trimmed/${filename2%%.*}_val_2.fq.gz
+else
+    read1=/c8000xd3/rnaseq-heath/Trimmed/${filename1%%.*}_val_1.fq
+    read2=/c8000xd3/rnaseq-heath/Trimmed/${filename2%%.*}_val_2.fq
+fi
 echo "Started processing $sampleID"
+echo $read1
+echo $read2
 mkdir /c8000xd3/rnaseq-heath/Mappings/$sampleID
 tophat --keep-fasta-order --library-type fr-secondstrand --mate-inner-dist 500 --mate-std-dev 50 --num-threads 8 \
   --transcriptome-index /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.inx \
   --output-dir /c8000xd3/rnaseq-heath/Mappings/$sampleID \
   /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/Bowtie2Index/genome \
-  /c8000xd3/rnaseq-heath/Trimmed/${filename1%%.*}_val_1.fq /c8000xd3/rnaseq-heath/Trimmed/${filename2%%.*}_val_2.fq
+  $read1 $read2
 mkdir /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM
 mv /c8000xd3/rnaseq-heath/Mappings/$sampleID/accepted_hits.bam /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/
 mv /c8000xd3/rnaseq-heath/Mappings/$sampleID/unmapped.bam /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/
