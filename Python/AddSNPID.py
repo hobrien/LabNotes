cbff18cbbd3dce2581e15a0e800beeeacf81de67
Heath O'Brien <heath.obrien@gmail.com>
2016-04-21 13:12:52 +0100

    Modified to add IDs rather than checking them against VCF
    IDs are identified by position (start and start plus ref length), if
    both ref and alt from the VCF (or there reverse-complements) are
    present among the observed variants. If there are multiple matching SNP
    IDs the first one in the DB is used. If the SNPs are flagged as
    duplicates, this is done silently, but a warning is printed if they are
    not listed as duplicates in the DB.
    
    In testing earlier version against the omni chip VCF, this worked in
    all cases except a few where the omnichip SNP ID has been discontinued
    and replaced by the DB one.

--- Python/AddSNPID.py
+++ Python/AddSNPID.py
@@ -6,6 +6,7 @@ import fileinput
 import warnings
 import mysql.connector 
 from mysql.connector import Error
+from string import maketrans
 
 # read header using bcftools, lookup sample IDs in DB, then write new header with BrainIDs
  
@@ -20,12 +21,20 @@ def main(args):
              
        matchingSNPs = 0
        for line in fileinput.input():
-           (chromosome, position, id, ref, alt) = line.strip().split('\t')
-           if id != GetID(cursor, chromosome, position, ref, alt):
-               warnings.warn("id %s at %s position %s does not match DB id (%s)" % (id, chromosome, position, GetID(cursor, chromosome, position, ref, alt)))
+           if line[0] == '#':
+               print line.strip()
+               continue
+           row = line.strip().split('\t')
+           (chromosome, position, vcf_id, ref, alt) =  row[:5]
+           if alt == '.':
+               continue
+           db_id = GetID(cursor, chromosome, position, ref, alt, id)
+           if db_id != 'NA':
+               row[2] = db_id
+           print '\t'.join(row)
            matchingSNPs += 1
-           if matchingSNPs % 1000 == 0:
-               warnings.warn("matched %i SNPs" % matchingSNPs)
+           #if matchingSNPs % 1000 == 0:
+           #    warnings.warn("matched %i SNPs" % matchingSNPs)
   
     except Error as e:
         print(e)
@@ -35,33 +44,44 @@ def main(args):
         conn.close()
     
     
-def GetID(cursor, chromosome, position, ref, alt):
+def GetID(cursor, chromosome, position, ref, alt, id):
     if chromosome[:3] != 'Chr':
         chromosome = "Chr%s" % chromosome
-    cursor.execute("SELECT name, observed FROM snp144 WHERE chrom = %s AND chromEnd = %s", (chromosome, position,))
+    cursor.execute("SELECT name, observed, exceptions FROM snp144 WHERE chrom = %s AND chromStart = %s AND chromEnd = %s", (chromosome, int(position)-1, int(position)-1+len(ref),))
     rows = cursor.fetchall()
+    
     rsID = []
-    for row in rows:
-        db = row[1].encode('ascii').replace('-', '.').split('/')
-        db.sort()
-        vcf = [ref, alt]
-        vcf.sort()
-        if db == vcf:
-            rsID.append(row[0])
+    if len(rows) == 0:
+        return 'NA'
+    elif len(rows) == 1:
+        rsID = [rows[0][0]]
+    else:    
+        for row in rows:
+            db = row[1].encode('ascii').replace('-', '.').split('/')
+            if ref in db:
+                if alt in db:
+                    rsID.append(row[0])
+            elif RevCom(ref) in db and RevCom(alt) in db:
+                rsID.append(row[0])
     try:
         assert len(rsID) == 1
     except AssertionError:
         if len(rsID) == 0:
-            warnings.warn("No SNP at %s position %s" % (chromosome, position))
+            warnings.warn("None of the %i SNPs at %s position %s match %s/%s" % (len(rows), chromosome, position, ref, alt))
             rsID = ['NA']
-        else:
+        elif 'DuplicateObserved' not in rows[0][2]:
             warnings.warn("%i rows matching %s position %s" % (len(rsID), chromosome, position))
     return  rsID[0]
 
+def RevCom(vcf):
+    intab = "ACGT"
+    outtab = "TGCA"
+    trantab = maketrans(intab, outtab)
+    vcf = vcf.translate(trantab)[::-1]
+    return vcf
+    
 def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
     return ' %s:%s: %s: %s\n' % (filename, lineno, category.__name__, message)
-
-
            
 if __name__ == "__main__":
     warnings.formatwarning = warning_on_one_line
