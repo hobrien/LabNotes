18a1a3a60836d513b1a6500f665daf83c1f09d27
Heath O'Brien <heath.obrien@gmail.com>
2016-04-28 14:48:26 +0100

    Merged edits on server

--- SubmissionScripts/FilterBAM.sh
+++ SubmissionScripts/FilterBAM.sh
@@ -15,11 +15,5 @@ samtools index /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.ex.sort.b
 samtools view -bh /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.ex.sort.bam \
 chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY \
 > /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.bam
-rm /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.ex.sort.bam /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.ex.sort.bam.bai
-#samtools view -H /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.bam |grep -E '@HD|@PG|chr[0-9]{1,2}\s' > /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/header.sam
-#samtools reheader /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/header.sam /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.bam \
-#> /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.reheader.bam
 samtools index /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.bam
-
-rm /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.reheader.bam /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/header.sam
-
+rm /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.ex.sort.bam /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.ex.sort.bam.bai
