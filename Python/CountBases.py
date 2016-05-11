3f2a2817a45d33d4d7b7425e510d46a0a87897cd
Heath O'Brien <heath.obrien@gmail.com>
2016-05-11 20:45:18 +0100

    added code to deal with position with zero reads mapped

--- Python/CountBases.py
+++ Python/CountBases.py
@@ -9,11 +9,13 @@ import warnings
 def main(args):
        for line in fileinput.input():
            fields = line.split()
-           bases = fields[4]
-           ref = bases.count(',')+bases.count('.')
-           alt = int(fields[3]) - ref - bases.count('<') - bases.count('>')
-           print '\t'.join(fields[:3]+ [str(ref), str(alt)])    
-               
+           try:
+               bases = fields[4]
+               ref = bases.count(',')+bases.count('.')
+               alt = int(fields[3]) - ref - bases.count('<') - bases.count('>')
+               print '\t'.join(fields[:3]+ [str(ref), str(alt)])    
+           except IndexError: # no pileup data
+               print '\t'.join(fields[:3]+ ['0', '0'])    
   
 def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
     return ' %s:%s: %s: %s\n' % (filename, lineno, category.__name__, message)
