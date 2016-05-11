d2d8e1657236803fd24f0f2979ede79dd14a1e9f
Heath O'Brien <heath.obrien@gmail.com>
2016-05-11 20:17:46 +0100

    script to run mpileup

---
+++ SubmissionScripts/mpileup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+# see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
+filename1=${1##*/}
+file_location = ${1%/*} 
+sampleID=${filename1%%.*}
+
+
+
+samtools mpileup -d 8000 \
+-f /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
+$@ > $file_location/$sampleID.mpileup.txt
