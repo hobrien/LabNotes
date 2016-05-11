84f685e199a33cdcb2c90f32a325cc2ee4a781cc
Heath O'Brien <heath.obrien@gmail.com>
2016-05-11 20:59:40 +0100

    modified to work with multiple files

--- SubmissionScripts/mpileup.sh
+++ SubmissionScripts/mpileup.sh
@@ -8,12 +8,15 @@
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
-filename1=${1##*/}
-file_location=${1%/*} 
-sampleID=${filename1%%.*}
+for input in $@
+do
+    filename1=${input##*/}
+    file_location=${input%/*} 
+    sampleID=${filename1%%.*}
 
 
 
-samtools mpileup -d 8000 \
--f /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
-$@ |python ~/LabNotes/Python/CountBases.py > $file_location/$sampleID.mpileup.txt
+    samtools mpileup -d 8000 \
+    -f /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
+    $@ |python ~/LabNotes/Python/CountBases.py > $file_location/$sampleID.mpileup.txt
+done
