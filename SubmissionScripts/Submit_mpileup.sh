cdc4c91bd094fc71f39fb8893823678ad67e58df
Heath O'Brien <heath.obrien@gmail.com>
2016-05-11 21:23:56 +0100

    removed line breaks between commands

--- SubmissionScripts/Submit_mpileup.sh
+++ SubmissionScripts/Submit_mpileup.sh
@@ -7,75 +7,75 @@ qsub ~/LabNotes/SubmissionScripts/mpileup.sh \ #/c8000xd3/rnaseq-heath/Mappings/
 /c8000xd3/rnaseq-heath/Mappings/15768/BAM/15768.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16286/BAM/16286.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16438/BAM/16438.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/16488/BAM/16488.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/16488/BAM/16488.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/16840/BAM/16840.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16929/BAM/16929.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16972/BAM/16972.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/17049/BAM/17049.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/17049/BAM/17049.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/17054/BAM/17054.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17068/BAM/17068.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17081/BAM/17081.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/17087/BAM/17087.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/17087/BAM/17087.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/17109/BAM/17109.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17115/BAM/17115.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17130/BAM/17130.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17701/BAM/17701.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/17812/BAM/17812.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/17812/BAM/17812.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/18294/BAM/18294.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18349/BAM/18349.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18596/BAM/18596.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18655/BAM/18655.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/18687/BAM/18687.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/18687/BAM/18687.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/15641/BAM/15641.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16024/BAM/16024.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16115/BAM/16115.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16385/BAM/16385.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/16428/BAM/16428.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/16428/BAM/16428.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/16491/BAM/16491.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16548/BAM/16548.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16810/BAM/16810.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16826/BAM/16826.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/17048/BAM/17048.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/17048/BAM/17048.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/17053/BAM/17053.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17071/BAM/17071.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17921-l1/BAM/17921-l1.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/15655/BAM/15655.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/16483/BAM/16483.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/16483/BAM/16483.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/16640/BAM/16640.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17013/BAM/17013.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17198/BAM/17198.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17229/BAM/17229.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/17333/BAM/17333.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/17333/BAM/17333.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/17475/BAM/17475.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17543/BAM/17543.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17629/BAM/17629.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17835/BAM/17835.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/18249/BAM/18249.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/18249/BAM/18249.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/18372/BAM/18372.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18666/BAM/18666.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18983/BAM/18983.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/19043/BAM/19043.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/19052/BAM/19052.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/19052/BAM/19052.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/A138/BAM/A138.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/A226/BAM/A226.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/15240/BAM/15240.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/16649/BAM/16649.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/17072/BAM/17072.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/17072/BAM/17072.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/17160/BAM/17160.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17167/BAM/17167.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17175/BAM/17175.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17369/BAM/17369.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/17671/BAM/17671.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/17671/BAM/17671.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/17922/BAM/17922.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/17923/BAM/17923.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18055/BAM/18055.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18121/BAM/18121.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/18134/BAM/18134.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/18134/BAM/18134.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/18153/BAM/18153.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18241/BAM/18241.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18266/BAM/18266.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18282/BAM/18282.chr.bam \
-/c8000xd3/rnaseq-heath/Mappings/18355/BAM/18355.chr.bam \
+/c8000xd3/rnaseq-heath/Mappings/18355/BAM/18355.chr.bam 
 qsub ~/LabNotes/SubmissionScripts/mpileup.sh /c8000xd3/rnaseq-heath/Mappings/15533_2/BAM/15533_2.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18432/BAM/18432.chr.bam \
 /c8000xd3/rnaseq-heath/Mappings/18559/BAM/18559.chr.bam \
