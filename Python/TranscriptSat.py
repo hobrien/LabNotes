4c5c402a7951aab9db3feaae20defe480520ea44
Heath O'Brien <heath.obrien@gmail.com>
2016-05-12 19:19:41 +0100

    Wrote python script to calculate saturation
    The R version was too slow to do on my laptop and the version of R on
    the server is too slow.

---
+++ Python/TranscriptSat.py
@@ -0,0 +1,16 @@
+import sys
+import pandas as pd
+import numpy as np
+
+Combined = pd.read_table(sys.argv[1], header=None)
+
+for index in range(4, len(Combined.columns)):
+    Combined[index] = np.where(Combined[index]=='-', 0, 1)
+
+for class_symbol in np.unique(Combined[3]):
+    subset = Combined[Combined[3] == class_symbol].ix[:,4:]
+    for n in range(1, len(subset.columns)):
+        sample = []
+        for x in range(100):
+            sample.append(np.count_nonzero(subset.sample(n=n, axis=1).sum(axis=1)))
+        print '\t'.join((class_symbol, str(n), str(sum(sample)/100.0)))
---
+++ SubmissionScripts/SubmitTrasnscriptSat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+python ~/LabNotes/Python/TranscriptSat.py $1
