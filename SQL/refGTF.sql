4f5a2d4fe819e6010046266604f5d97b1d850497
Heath O'Brien <heath.obrien@gmail.com>
2016-05-24 09:48:30 +0100

    Loaded Gencode GTF to DB and used to examine size dist of lncRNAs
    - Cufflinks-specific version of GTF2CSV.py renamed Cufflinks2CSV.py
    - Generic GTF2CSV.py tool written that writes first 8 columns to STD
    out and attribute column to file called features.csv where each
    attribute in on a separate row, with columns for feature id, attribute
    name and attribute value
    - SQL to create DB tables and load data, along with the output of this
    Query:
    - “SELECT GencodeFeatures.`value`, COUNT(GencodeFeatures.`value`) FROM
    GencodeFeatures, GencodeGTF WHERE GencodeFeatures.`id` =
    GencodeGTF.`id` AND GencodeFeatures.`feature` = 'gene_type' AND
    GencodeGTF.`feature` = 'gene' GROUP BY GencodeFeatures.`value`”
    - rmd to examine distributions of lncRNA exon lengths
    - I still need to figure out a way to get the total length of all exons
    for each lncRNA to see if I can apply a minimum length cutoff

---
+++ SQL/refGTF.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS `GencodeGTF`;
+CREATE TABLE `GencodeGTF` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `seqid` varchar(255) DEFAULT NULL,
+  `source` varchar(255) DEFAULT NULL,
+  `feature` varchar(255) DEFAULT NULL,
+  `start` int(11) DEFAULT NULL,
+  `end` int(11) DEFAULT NULL,
+  `score` float DEFAULT NULL,
+  `strand` char(1) DEFAULT NULL,
+  `frame` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+LOAD DATA INFILE '/Users/heo3/BTSync/FetalRNAseq/Reference/gencode.v24.chr_patch_hapl_scaff.annotation.csv'
+INTO TABLE GencodeGTF  
+    FIELDS TERMINATED BY ',' 
+           OPTIONALLY ENCLOSED BY '"'
+    LINES  TERMINATED BY '\n' -- or \r\n
+    (id,
+     seqid, 
+     source, 
+     feature, 
+     start, 
+     end, 
+     score, 
+     strand, 
+     frame
+     );
+ALTER TABLE `GencodeGTF` ADD INDEX (`seqid`, `start`, `end`);
+ALTER TABLE `GencodeGTF` ADD INDEX (`feature`);
+
+
+DROP TABLE IF EXISTS `GencodeFeatures`;
+CREATE TABLE `GencodeFeatures` (
+  `id` int(11) unsigned NOT NULL,
+  `feature` varchar(255) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`, `feature`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOAD DATA INFILE '/Users/heo3/BTSync/FetalRNAseq/Reference/gencode.v24.chr_patch_hapl_scaff.features.csv'
+INTO TABLE `GencodeFeatures`  
+    FIELDS TERMINATED BY ',' 
+           OPTIONALLY ENCLOSED BY '"'
+    LINES  TERMINATED BY '\n' -- or \r\n
+    (`id`,
+     `feature`, 
+     `value`
+     );
+ALTER TABLE `GencodeFeatures` ADD INDEX (`value`);
+ALTER TABLE `GencodeFeatures` ADD INDEX (`feature`);
+
+
