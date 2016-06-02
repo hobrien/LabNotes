a6d3a8c25e9330b092b7d5023284a08cf4794153
Heath O'Brien <heath.obrien@gmail.com>
2016-06-02 14:51:28 +0100

    modified liftover to work on all chromosomes

--- SubmissionScripts/Liftover.sh
+++ SubmissionScripts/Liftover.sh
@@ -10,9 +10,14 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 #path=${1%/*}
 #sampleID=${path##*/}
-
+bcftools view /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$1.dose.vcf.gz \
+  |perl -pe 's/^(\n+)/chr$1/' \
+  | bgzip \
+  > /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$1.recoded.vcf.gz
 CrossMap.py vcf /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/hg19ToHg38.over.chain \
-  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr1.recoded.vcf.gz \
+  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$1.recoded.vcf.gz \
   /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
-  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr1.GRCh38.vcf
-bgzip /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr1.GRCh38.vcf
+  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$1.GRCh38.vcf
+bgzip /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$1.GRCh38.vcf
+rm /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$1.GRCh38.vcf 
+rm /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$1.recoded.vcf.gz
---
+++ SubmissionScripts/SubmitLiftover.sh
@@ -0,0 +1,4 @@
+for num in {3..25}
+do
+    qstat ~/LabNotes/SubmissionScripts/Liftover.sh $num
+done
