c6e663d7f7a2309e5c5f8290018e2ad49befbdc9
Heath O'Brien <heath.obrien@gmail.com>
2016-06-08 09:42:30 +0100

    removed extra line

--- SubmissionScripts/Cuffquant.sh
+++ SubmissionScripts/Cuffquant.sh
@@ -16,7 +16,6 @@ echo "Starting Cuffquant on $sampleID"
 cuffquant -p 8 -u -library-type fr-secondstrand \
   -o /c8000xd3/rnaseq-heath/Cufflinks/Cuffquant \
   -b /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
-  /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
   /c8000xd3/rnaseq-heath/Cufflinks/Cuffmerge/merged.gtf \
   /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.bam
 echo "Finished Cuffquant on $sampleID"
