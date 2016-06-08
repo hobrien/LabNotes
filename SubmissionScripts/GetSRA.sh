fce3921ac75f414b392883adfd7b7ea9f8e45b96
Heath O'Brien <heath.obrien@gmail.com>
2016-06-08 13:16:23 +0100

    wrote script to download sra data
    to install:
    cd ~/src
    
    wget
    "http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ce
    ntos_linux64.tar.gz"
    
    tar -xzf sratoolkit.current-centos_linux64.tar.gz
    
    ~/src/sratoolkit.2.6.3-centos_linux64/bin/vdb-config -i #change
    directory to /c8000xd3/rnaseq-heath/ncbi/public/sra/
    
    ln -s ~/src/sratoolkit.2.6.3-centos_linux64/bin/fastq-dump
    ~/bin/fastq-dump

---
+++ SubmissionScripts/GetSRA.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+# see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
+#path=${1%/*}
+sraID=$1
+
+echo "Starting SRA dump for $sraID"
+
+fastq-dump --split-files --gzip --readids $sraID
+echo "Finished SRA dump for $sraID. Output in /c8000xd3/rnaseq-heath/ncbi/public/src"
