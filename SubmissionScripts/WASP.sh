14945e4d22d2b00e18452312bb915407814f4316
hobrien <heath.obrien@gmail.com>
2016-06-14 13:37:49 +0100

    modified to work on specified file

--- SubmissionScripts/WASP.sh
+++ SubmissionScripts/WASP.sh
@@ -10,4 +10,4 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 #path=${1%/*}
 #sampleID=${path##*/}
-python ~/src/WASP/mapping/find_intersecting_snps.py -p /c8000xd3/rnaseq-heath/Mappings/15533/BAM/15533.chr.bam /c8000xd3/rnaseq-heath/Genotypes/Imputation2/SNPs/
+python ~/src/WASP/mapping/find_intersecting_snps.py -p $1 /c8000xd3/rnaseq-heath/Genotypes/Imputation2/SNPs/
