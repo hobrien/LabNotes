51e4195da9fe8c518a0c349997614567766dfb18
hobrien <heath.obrien@gmail.com>
2016-06-16 19:55:13 +0100

    fixed path parsing

--- SubmissionScripts/WASP.sh
+++ SubmissionScripts/WASP.sh
@@ -9,6 +9,7 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 path=${1%/*}
+path=${path%/*}
 sampleID=${path##*/}
 
 echo "Starting WASP Remapping on $sampleID"
