43820f5e202dc28a07b0adfae5dd4066cddd8f54
Heath O'Brien <heath.obrien@gmail.com>
2016-06-21 15:27:20 +0100

    Modified to work on a single sample
    Also modified to loop over all chromosomes. Previously I used
    SubmitExtractSNPs.sh to loop over chromosomes and submit each as a
    separate job, but that seems like overkill if I also need to loop over
    samples.

--- SubmissionScripts/ExtractSNPs.sh
+++ SubmissionScripts/ExtractSNPs.sh
@@ -7,7 +7,11 @@
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
-# see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
-#path=${1%/*}
-#sampleID=${path##*/}
-bcftools view -H  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$1.GRCh38.vcf.gz | cut -f2,4,5 |gzip -c > /c8000xd3/rnaseq-heath/Genotypes/Imputation2/SNPs/chr$1.snps.txt.gz
+# Index is the position of the sample in the VCF. Name is the BrainBankID
+sampleName=$1
+sampleIndex=$2
+
+for num in {1..21}
+do
+    bcftools view -H  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$num.GRCh38.vcf.gz | cut -f1,2,3,4,5,6,7,8,9,$sampleIndex | grep -v '0|0' | cut -f2,4,5 |gzip -c > /c8000xd3/rnaseq-heath/Genotypes/Imputation2/$sampleName/chr$num.snps.txt.gz
+done
