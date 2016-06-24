ed08f40a34b494ba119ca6aa4a9156d72ee0faba
Heath O'Brien <heath.obrien@gmail.com>
2016-06-24 15:30:56 +0100

    fixed problems with path names

--- SubmissionScripts/htseq-count.sh
+++ SubmissionScripts/htseq-count.sh
@@ -10,12 +10,13 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 filename=${1##*/}
-sampleID=${filename%%.bam}
+filename=${filename%%.bam}
+sampleID=$[filename%%.*}
 
-samtools sort -n /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.bam \
-  /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.nsort 
+samtools sort -n /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.bam \
+  /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.nsort 
   
 htseq-count -f bam -s reverse -t exon -i gene_id -m intersection-strict \
-  /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.nsort.bam \
+  /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.nsort.bam \
   /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
-  > /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.counts.txt
+  > /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.counts.txt
