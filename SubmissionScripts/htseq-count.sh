4a098da2073a44d240a8e8eaa49fae7e50fd9ba2
Heath O'Brien <heath.obrien@gmail.com>
2016-06-24 15:33:35 +0100

    added logging notes

--- SubmissionScripts/htseq-count.sh
+++ SubmissionScripts/htseq-count.sh
@@ -11,8 +11,9 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 filename=${1##*/}
 filename=${filename%%.bam}
-sampleID=$[filename%%.*}
+sampleID=${filename%%.*}
 
+echo "Getting counts for $filename"
 samtools sort -n /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.bam \
   /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.nsort 
   
@@ -20,3 +21,4 @@ htseq-count -f bam -s reverse -t exon -i gene_id -m intersection-strict \
   /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.nsort.bam \
   /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
   > /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.counts.txt
+echo "Finished getting counts for $filename"
