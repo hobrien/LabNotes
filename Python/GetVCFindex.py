8214391bd36db80011dac4967433a95bc75336d4
Heath O'Brien <heath.obrien@gmail.com>
2016-07-07 17:18:56 +0100

    changed index counting
    numbers in VCF header repeat so I had to use a counter to get global
    numbers
    
    This is a hell of a lot safer anyway

--- Python/GetVCFindex.py
+++ Python/GetVCFindex.py
@@ -17,13 +17,20 @@ def main(args):
                                        user='root')
             
        cursor = conn.cursor()
-             
+       index = 0      
        for line in fileinput.input():
            (pos, sentrix1, sentrix2) = line.strip().split('_')
-           cursor.execute("SELECT BrainBankID FROM PC_analysis WHERE Sentrix_Full = %s", ('_'.join((sentrix1,sentrix2)),))
+           index += 1
+           sid = '_'.join((sentrix1,sentrix2))
+           cursor.execute("SELECT BrainBankID FROM PC_analysis WHERE Sentrix_Full = %s", (sid,))
            rows = cursor.fetchall()
-           assert len(rows) == 1
-           print '\t'.join((rows[0][0], str(int(pos)+9)))
+           if len(rows) == 0:
+               warnings.warn("No DB entry for %s" % sid)
+               sys.exit()
+           elif len(rows) > 1:    
+               warnings.warn("Multiple DB entry for %s" % sid)
+               sys.exit()
+           print '\t'.join((rows[0][0], str(int(index)+9)))
   
     except Error as e:
         print(e)
