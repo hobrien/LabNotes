c0d55702868743a53dcca5c12e018ff2cee7532c
hobrien <heath.obrien@gmail.com>
2016-07-13 13:53:48 +0100

    added progress logging

--- SubmissionScripts/FastQC.sh
+++ SubmissionScripts/FastQC.sh
@@ -4,6 +4,9 @@
 #$ -j y
 #$ -S /bin/bash
 #
+echo "running fastQC on $@"
 function joinStrings { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
 #zcat '@$' | ~/src/FastQC/fastqc --outdir=/home/mpmho/FastQC stdin
 ~/src/FastQC/fastqc --outdir=/home/mpmho/FastQC "$@"
+
+echo "finished running fastQC on $@"
