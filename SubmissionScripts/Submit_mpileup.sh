9b06d174af32c1477f8e6203a6018c5f4a8d0736
Heath O'Brien <heath.obrien@gmail.com>
2016-07-13 16:46:48 +0100

    need to give range of coords

--- SubmissionScripts/Submit_mpileup.sh
+++ SubmissionScripts/Submit_mpileup.sh
@@ -7,15 +7,15 @@
 
 export PATH=/share/apps/R-3.2.2/bin:$PATH
 
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr2:184936178 #ZNF804A
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr1:98046571 #MIR137HG
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr2:192775750 #PCGEM1
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr6:30186422 #TRIM26
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr8:2937908 #CSMD1
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr8:88039069 #MMP16
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr10:103054405 #CNNM2
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr10:103089711 #NT5C2
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr11:125609468 #STT3A
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr18:54903114 #CCDC68
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr18:55228300 #TCF4
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr2:184936178-184936178 #ZNF804A
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr1:98046571-98046571 #MIR137HG
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr2:192775750-192775750 #PCGEM1
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr6:30186422-30186422 #TRIM26
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr8:2937908-2937908 #CSMD1
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr8:88039069-88039069 #MMP16
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr10:103054405-103054405 #CNNM2
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr10:103089711-103089711 #NT5C2
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr11:125609468-125609468 #STT3A
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr18:54903114-54903114 #CCDC68
+bash ~/LabNotes/SubmissionScripts/mpileup.sh chr18:55228300-55228300 #TCF4
 
