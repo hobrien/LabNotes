29f686aaac6d2b9bcebeea875da6caab55d546d3
hobrien <heath.obrien@gmail.com>
2016-07-25 15:08:35 +0100

    changed to local copy of samtools, which is newer

--- SubmissionScripts/CallSNPs.sh
+++ SubmissionScripts/CallSNPs.sh
@@ -7,9 +7,10 @@
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
-
-samtools mpileup -uf  /home/mpmho/Ref/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa $@ \
-   | bcftools view -bvcg - > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/var.raw.bcf  
+# this command will call SNPs on all smaple (except 15533 dup):
+# find /c8000xd3/rnaseq-heath/Mappings/ -name *chr.nonref.merged.sorted.bam | grep -v SRR | grep -v 50bp | grep -v _ | xargs qsub ../LabNotes/SubmissionScripts/CallSNPs.sh
+~/bin/samtools mpileup -uf /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa $@ \
+   | bcftools call -mv -o b - > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/var.raw.bcf  
    
 bcftools view /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/var.raw.bcf \
    | vcfutils.pl varFilter -D1000 > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/var.flt.vcf 
