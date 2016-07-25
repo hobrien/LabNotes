b6c2c1f10c7214ef9403a246d13006a195bc2964
hobrien <heath.obrien@gmail.com>
2016-07-25 17:05:14 +0100

    added actual code to run samtools.
    I also modified the find command becuase it was not returning the bam files

--- SubmissionScripts/ExtractHTTbam.sh
+++ SubmissionScripts/ExtractHTTbam.sh
@@ -1,10 +1,11 @@
-# command to extract from all samples: find /c8000xd3/rnaseq-heath/Mappings/ -maxdepth 2 -name junctions.bed | grep -v SRR | grep -v 50bp | grep -v _ | xargs bash SubmissionScripts/ExtractHTTjuncitons.bed
+# command to extract from all samples: find /c8000xd3/rnaseq-heath/Mappings/ -maxdepth 3 -name *.chr.bam | grep -v SRR | grep -v 50bp | grep -v _ | xargs bash SubmissionScripts/ExtractHTTjuncitons.bed
 
 
 for dataset in $@
 do
     folder_path=${dataset%/*}
+    folder_path=${folder_path%/*}
     sampleID=${folder_path##*/}
 
-    echo $sampleID
+    samtools view -b $dataset chr4:3074680-3243960 >~/HTT/$sampleID.htt.bam
 done
