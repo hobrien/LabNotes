ecfbf86542882cb7fdd87ecf65bbe2cbb4b0b97d
hobrien <heath.obrien@gmail.com>
2016-07-25 17:02:32 +0100

    modifed to use the name of the first input file in the outfile name
    - The first bit is stripped away (up to the first .)
    - As long as this part specifies the sampleID, this should give an informative name

--- SubmissionScripts/CallSNPs.sh
+++ SubmissionScripts/CallSNPs.sh
@@ -9,8 +9,16 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 # this command will call SNPs on all smaple (except 15533 dup):
 # find /c8000xd3/rnaseq-heath/Mappings/ -name *chr.nonref.merged.sorted.bam | grep -v SRR | grep -v 50bp | grep -v _ | xargs qsub ../LabNotes/SubmissionScripts/CallSNPs.sh
+
+#Need to remove sampleID and .bam from first input and use the rest to name the output file
+#this will make it possible to call SNPs on each chromosome individually
+
+# see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
+filename=${1##*/}
+filename=${filename#*.}
+filename=${filename%%.bam}
 ~/bin/samtools mpileup -uf /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa $@ \
-   | bcftools call -mv -o b - > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/var.raw.bcf  
+   | bcftools call -mv -o b - > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$filename.raw.bcf
    
-bcftools view /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/var.raw.bcf \
-   | vcfutils.pl varFilter -D1000 > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/var.flt.vcf 
+#bcftools view /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/var.raw.bcf \
+#   | vcfutils.pl varFilter -D1000 > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$filename.filt.vcf
