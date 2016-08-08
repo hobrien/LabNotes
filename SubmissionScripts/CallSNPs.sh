cdf432e8badd9fd318617283dded7ceb8436d2ee
hobrien <heath.obrien@gmail.com>
2016-08-08 12:00:02 +0100

    got SNP calling working, but the filtering step sill fails. It also quit without finishing on almost all chromosomes. chr22 is the only one that worked

--- SubmissionScripts/CallSNPs.sh
+++ SubmissionScripts/CallSNPs.sh
@@ -10,15 +10,15 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 # this command will call SNPs on all smaple (except 15533 dup):
 # find /c8000xd3/rnaseq-heath/Mappings/ -name *chr.nonref.merged.sorted.bam | grep -v SRR | grep -v 50bp | grep -v _ | xargs qsub ../LabNotes/SubmissionScripts/CallSNPs.sh
 
-#Need to remove sampleID and .bam from first input and use the rest to name the output file
-#this will make it possible to call SNPs on each chromosome individually
+# Based on: https://github.com/samtools/bcftools/wiki/HOWTOs#mpileup-calling
 
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 filename=${1##*/}
 filename=${filename#*.}
 filename=${filename%%.bam}
-~/bin/samtools mpileup -uf -d 100000 /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa $@ \
-   | bcftools call -mv -o b - > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$filename.raw.bcf
+#~/bin/samtools mpileup -uf /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa /c8000xd3/rnaseq-heath/Mappings/15240/BAM/Chromosomes/15240.chr22.bam | ~/bin/bcftools call -mv -Ob > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/chr22.raw.bcf
+~/bin/samtools mpileup -uf /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa $@ \
+   | ~/bin/bcftools call -mv -Ob > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$filename.raw.bcf
    
-#bcftools view /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/var.raw.bcf \
-#   | vcfutils.pl varFilter -D1000 > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$filename.filt.vcf
+bcftools view /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$filename.raw.bcf \
+   | vcfutils.pl varFilter -D1000 > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$filename.filt.vcf
