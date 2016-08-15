cbbb45b6b51a7e97d8205a76539782cbfd12b80d
hobrien <heath.obrien@gmail.com>
2016-08-15 11:35:29 +0100

    changed order of arguments

--- SubmissionScripts/dexseq-count.sh
+++ SubmissionScripts/dexseq-count.sh
@@ -16,7 +16,7 @@ sampleID=${filename%%.*}
 echo "Getting counts for $filename"
 
 dexseq_count.py -p yes -r name -s reverse -f bam \
-  $1 \
   /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gff \
-  > /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.dex_counts.txt
+  $1 \
+  /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.dex_counts.txt
 echo "Finished getting counts for $filename"
