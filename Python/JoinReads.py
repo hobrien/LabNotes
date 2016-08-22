53c64e6c7cbe583ecd083d285d99216f4cf02af7
Heath O'Brien <heath.obrien@gmail.com>
2016-08-22 12:48:54 +0100

    removed commas from start of output
    I thought I could get away without using the join function, but as
    usual, it’s easier to do it right

--- Python/JoinReads.py
+++ Python/JoinReads.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import sys
 
-read1 = ''
-read2 = ''
+read1 = []
+read2 = []
 for i in range(1, len(sys.argv)-1, 2):
-    read1 = read1 + ',' + sys.argv[i]
-    read2 = read2 + ',' + sys.argv[i+1]
+    read1 += sys.argv[i:i+1]
+    read2 += sys.argv[i+1:i+2]
     
-print read1, read2
+print ','.join(read1), ','.join(read2)
 
         
