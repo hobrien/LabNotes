186cc4aaa086b124cf91606d21414ff3689d7547
hobrien <heath.obrien@gmail.com>
2016-08-31 11:24:52 +0100

    added newline character to last line

--- SubmissionScripts/htseq-count.sh
+++ SubmissionScripts/htseq-count.sh
@@ -22,3 +22,4 @@ htseq-count -f bam -s reverse -t exon -i gene_id -m intersection-strict \
   /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
   > /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$filename.counts.txt
 echo "Finished getting counts for $filename"
+
