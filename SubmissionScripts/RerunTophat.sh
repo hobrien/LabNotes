99aaf807de20b9be5ecf3718644d18a959dc030c
Heath O'Brien <heath.obrien@gmail.com>
2016-09-02 18:51:21 +0100

    rerun the steps that I screwed up last time

---
+++ SubmissionScripts/RerunTophat.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+sampleID=$1
+bash ~/LabNotes/SubmissionScripts/CallSNPs.sh /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/Chromosomes/$sampleID.chr22.bam
+bash ~/SubmissionScripts/GTcheck.sh $sampleID
+index=`grep $sampleID ~/LabNotes/VCFindex.txt | cut -f 2`
+bash ~/LabNotes/SubmissionScripts/WASPnonRef.sh $sampleID $index
+bash ~/LabNotes/SubmissionScripts/RNAseqQCwasp.sh /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.nonref.merged.sorted.bam
+bash ~/LabNotes/SubmissionScripts/RNAseqQCwasp.sh /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.nonref.merged.dedup.bam
+bash ~/LabNotes/SubmissionScripts/clipOverlap.sh /c8000xd3/rnaseq-heath/Mappings/$sampleID/BAM/$sampleID.chr.nonref.merged.dedup.bam
