46abeced8bd1e901369c36da2f2992b02be4ee45
Heath O'Brien <heath.obrien@gmail.com>
2016-09-03 09:14:08 +0200

    index bcf file

--- SubmissionScripts/GTcheck.sh
+++ SubmissionScripts/GTcheck.sh
@@ -11,4 +11,5 @@ refID=${1%-2}
 echo $refID > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.txt
 bcftools reheader -s /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.txt -o /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.chr22.renamed.bcf /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.chr22.raw.bcf 
 rm /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.txt 
+bcftools index /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.chr22.renamed.bcf
 bcftools gtcheck -g /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr22.GRCh38.renamed.vcf.gz -p chr22_$1 -s $1 -S $refID /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.chr22.renamed.bcf
