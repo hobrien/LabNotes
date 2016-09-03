08d02d8c4be93b1e3c9b456db84bdb816794d874
Heath O'Brien <heath.obrien@gmail.com>
2016-09-03 09:11:11 +0200

    rename samples in VCF

--- SubmissionScripts/GTcheck.sh
+++ SubmissionScripts/GTcheck.sh
@@ -8,4 +8,6 @@
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 refID=${1%-2}
-bcftools gtcheck -g /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr22.GRCh38.renamed.vcf.gz -p chr22_$1 -s $1 -S $refID /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/chr22.renamed.bcf
+echo $refID > /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.txt
+bcftools reheader -s /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.txt -o /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.chr22.renamed.bcf /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.chr22.raw.bcf 
+bcftools gtcheck -g /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr22.GRCh38.renamed.vcf.gz -p chr22_$1 -s $1 -S $refID /c8000xd3/rnaseq-heath/Genotypes/SNPcalls/$1.chr22.renamed.bcf
