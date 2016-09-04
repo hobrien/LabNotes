e9e9327ca59743a11031c721f7fb8a0b134d1fef
Heath O'Brien <heath.obrien@gmail.com>
2016-09-04 21:03:27 +0200

    change map Q cutoff

--- SubmissionScripts/QoRTs.sh
+++ SubmissionScripts/QoRTs.sh
@@ -9,6 +9,7 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 java -jar ~/src/QoRTs.jar QC \
 --stranded \
+--minMAPQ 50 \
 $1 \
 /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
 /c8000xd3/rnaseq-heath/Carolina
