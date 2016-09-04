a1ffde2ef4fecfac0ec25178a1cabf8b919b5b4e
Heath O'Brien <heath.obrien@gmail.com>
2016-09-04 21:00:14 +0200

    added script to run QoRTs.sh

---
+++ SubmissionScripts/QoRTs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+java -jar ~/src/QoRTs.jar QC \
+--stranded \
+$1 \
+/c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
+/c8000xd3/rnaseq-heath/Carolina
