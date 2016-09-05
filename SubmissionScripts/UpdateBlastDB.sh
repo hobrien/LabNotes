ff207fb153a96c61e9fb16e9f4d5ef622a121c4a
hobrien <heath.obrien@gmail.com>
2016-09-05 15:08:00 +0100

    uncompress DB files

--- SubmissionScripts/UpdateBlastDB.sh
+++ SubmissionScripts/UpdateBlastDB.sh
@@ -9,4 +9,4 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 export PERL5LIB=/home/mpmho/perl5/lib/perl5:/usr/lib64/perl5
 cd /c8000xd3/rnaseq-heath/DB
 ~/src/ncbi-blast-2.3.0+/bin/update_blastdb.pl nt
-gunzip /c8000xd3/rnaseq-heath/DB/*
+bash ~/LabNotes/SubmissionScripts/Untar.sh *.tar.gz
