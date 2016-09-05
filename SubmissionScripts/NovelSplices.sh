849e6467f3dbbf2fffe05521da778154ebc50070
Heath O'Brien <heath.obrien@gmail.com>
2016-09-05 15:04:56 +0200

    add code and info to find novel splice sites

---
+++ SubmissionScripts/NovelSplices.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+
+java -jar ~/src/QoRTs.jar QC \
+                mergeNovelSplices  \
+                --minCount 6 \
+                --stranded \
+                /c8000xd3/rnaseq-heath/Carolina/Counts/ \
+                ~/LabNotes/Carolina_sampleInfo.txt \
+                /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode/genes.gtf \
+                /c8000xd3/rnaseq-heath/Carolina/Counts/
+                
