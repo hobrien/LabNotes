e8f7625d626837bb6c4c9b6c304474b6575f2555
Heath O'Brien <heath.obrien@gmail.com>
2016-09-06 12:38:41 +0100

    run JunctionSeq on rocks

--- R/JunctionSeq.R
+++ R/JunctionSeq.R
@@ -1,21 +1,10 @@
 library(JunctionSeq)
 
-decoder <- read.table("~/Downloads/JctSeqData/inst/extdata/annoFiles/decoder.bySample.txt",
+decoder <- read.table("~/LabNotes/Carolina/decoder.bySample.txt",
                       header=TRUE,
                       stringsAsFactors=FALSE);
-gff.file <- "~/Downloads/JctSeqData/inst/extdata/cts/withNovel.forJunctionSeq.gff.gz"
-countFiles.noNovel<-"~/Downloads/JctSeqData/inst/extdata/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt.gz"
-countFiles<- "~/Downloads/JctSeqData/inst/extdata/QC.spliceJunctionAndExonCounts.withNovel.forJunctionSeq.txt.gz"
-gff.file<-"~/Downloads/JctSeqData/inst/extdata/tiny/withNovel.forJunctionSeq.gff.gz"
-countFiles<- paste0("~/Downloads/JctSeqData/inst/extdata/tiny/",
-                    decoder$sample.ID,
-                    "/QC.spliceJunctionAndExonCounts.withNovel.forJunctionSeq.txt.gz")
-
-decoder <- read.table("~/BTSync/FetalRNAseq/Counts/Carolina/decoder.bySample.txt",
-                      header=TRUE,
-                      stringsAsFactors=FALSE);
-gff.file <- "~/BTSync/FetalRNAseq/Counts/Carolina/withNovel.forJunctionSeq.gff.gz"
-countFiles<- paste0("~/BTSync/FetalRNAseq/Counts/Carolina/",
+gff.file <- "/c8000xd3/rnaseq-heath/Carolina/Counts/withNovel.forJunctionSeq.gff.gz"
+countFiles<- paste0("/c8000xd3/rnaseq-heath/Carolina/Counts/",
                     decoder$sample.ID,
                     "/QC.spliceJunctionAndExonCounts.withNovel.forJunctionSeq.txt.gz")
 
@@ -23,28 +12,17 @@ jscs <- runJunctionSeqAnalyses(sample.files = countFiles,
                                sample.names = decoder$sample.ID,
                                condition=factor(decoder$group.ID),
                                flat.gff.file = gff.file,
-                               nCores = 4,
+                               nCores = 8,
                                analysis.type = "junctionsAndExons"
 )
 
 buildAllPlots(jscs=jscs,
-              gene.list = c("ENSG00000151067.20", 
-                            "ENSG00000256837.1", 
-                            "ENSG00000256257.1", 
-                            "ENSG00000256025.1", 
-                            "ENSG00000256721.1", 
-                            "ENSG00000256769.1",
-                            "ENSG00000246627.6"),
-              outfile.prefix = "~/BTSync/FetalRNAseq/Counts/Carolina/plotsJCT-2/",
+              outfile.prefix = "/c8000xd3/rnaseq-heath/Carolina/plotsJCT-2/",
               use.plotting.device = "png",
               FDR.threshold = 0.01)
 
-buildAllPlotsForGene(jscs=jscs,
-                     geneID = "ENSG00000151067.20",
-              outfile.prefix = "~/BTSync/FetalRNAseq/Counts/Carolina/plotsJCT_genes/",
+buildAllPlots(jscs=jscs,
+              outfile.prefix = "/c8000xd3/rnaseq-heath/Carolina/plotsJCT-3/",
               use.plotting.device = "png",
-              expr.plot = FALSE, normCounts.plot = FALSE,
-              rExpr.plot = TRUE, rawCounts.plot = FALSE,
-              without.TX=FALSE,
-              colorList = list(SIG.FEATURE.FILL.COLOR = "red"),
-              colorRed.FDR.threshold = 0.001)
+              FDR.threshold = 0.001)
+save.image(file="/c8000xd3/rnaseq-heath/Carolina/junctionSeq.RData")
