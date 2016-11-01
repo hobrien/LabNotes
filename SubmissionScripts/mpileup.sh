3fdeae2420819f10890781f9aecf796213b924f9
Heath O'Brien <heath.obrien@gmail.com>
2016-11-01 11:28:35 +0000

    change to work with clipped reads

--- SubmissionScripts/mpileup.sh
+++ SubmissionScripts/mpileup.sh
@@ -8,7 +8,7 @@
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
-for input in `find /c8000xd3/rnaseq-heath/Mappings/ -name *.chr.nonref.merged.dedup.sort.bam`
+for input in `find /c8000xd3/rnaseq-heath/Mappings/ -name *.chr.nonref.merged.dedup.sort.clip.bam`
 do
     filename1=${input##*/}
     file_location=${input%/*} 
@@ -19,6 +19,6 @@ do
 
     samtools mpileup -d 8000 -r $1 \
     -f /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
-    $input |python ~/LabNotes/Python/CountBases.py $sampleID >> /c8000xd3/rnaseq-heath/Counts/$1.counts.txt
+    $input |python ~/LabNotes/Python/CountBases.py $sampleID >> /c8000xd3/rnaseq-heath/Counts/$1.clip.counts.txt
     echo "Finished processing $sampleID"
 done
