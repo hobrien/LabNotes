36b2781df308d799d4dac4ec0aa3aa9334ee6296
Heath O'Brien <heath.obrien@gmail.com>
2016-11-09 13:39:59 +0000

    added folder for outfile

--- SubmissionScripts/ASEReadCounter.sh
+++ SubmissionScripts/ASEReadCounter.sh
@@ -10,7 +10,7 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 filename=${1##*/}
 sampleID=${filename%%_*} #This will remove the library ID, lane number and read number
-outfilename=$sampleID.ase.rtable
+outfilename=/c8000xd3/rnaseq-heath/Counts/$sampleID.ase.rtable
 
 java -jar /share/apps/GenomeAnalysisTK.jar -T ASEReadCounter \
    -R /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
