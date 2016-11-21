8fd74043ece451ccf32b69b8011e48a6230bf6fe
Heath O'Brien <heath.obrien@gmail.com>
2016-11-21 15:30:00 +0000

    modified to only extract SNPs from chr22
    I skipped chr22 when I originally ran this, so I need to add it in now

--- SubmissionScripts/ExtractSNPs.sh
+++ SubmissionScripts/ExtractSNPs.sh
@@ -12,8 +12,9 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 sampleName=$1
 sampleIndex=$2
 
-mkdir  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/$sampleName/
-for num in {1..22}
-do
+num=22
+#mkdir  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/$sampleName/
+#for num in {1..22}
+#do
     bcftools view -H  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/chr$num.GRCh38.vcf.gz | cut -f1,2,3,4,5,6,7,8,9,$sampleIndex | grep -v '0|0' | cut -f2,4,5 |gzip -c > /c8000xd3/rnaseq-heath/Genotypes/Imputation2/$sampleName/chr$num.snps.txt.gz
-done
+#done
