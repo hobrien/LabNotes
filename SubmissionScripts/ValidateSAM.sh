0cac36e70358074a784537085abc33e4098e3289
Heath O'Brien <heath.obrien@gmail.com>
2016-11-28 12:39:18 +0000

    modified to run on filename passed from command line

--- SubmissionScripts/ValidateSAM.sh
+++ SubmissionScripts/ValidateSAM.sh
@@ -7,4 +7,4 @@
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
-java -Xmx2g -jar ~/src/picard-tools-1.139/picard.jar ValidateSamFile I=/c8000xd3/rnaseq-heath/Mappings/15240/BAM/15240.chr.nonref.merged.dedup.bam MODE=SUMMARY
+java -Xmx2g -jar ~/src/picard-tools-1.139/picard.jar ValidateSamFile I=$1 MODE=SUMMARY
