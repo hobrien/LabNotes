9f65449790ffe53c3d58d285ca18dccf437c6fee
Heath O'Brien <heath.obrien@gmail.com>
2016-11-29 11:58:21 +0000

    fixed typo

--- SubmissionScripts/AddRG.sh
+++ SubmissionScripts/AddRG.sh
@@ -9,7 +9,7 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 java -Xmx2g -jar ~/src/picard-tools-1.139/picard.jar AddOrReplaceReadGroups \
       I=$1 \
-      O=O=${1%.*}_RG.bam \
+      O=${1%.*}_RG.bam \
       RGID=$3 \
       RGLB=lib1 \
       RGPL=illumina \
