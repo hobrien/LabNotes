7850781b0a0de45273f3e13150470a21765a08b3
Heath O'Brien <heath.obrien@gmail.com>
2016-11-29 11:32:32 +0000

    loop over all SNPs for three mappings per sample
    These scripts should create separate count files for wasp-remapped
    alignments, deduplicated remapped alignments and reduplicated
    alignments with mate overlaps removed

--- SubmissionScripts/Submit_mpileup.sh
+++ SubmissionScripts/Submit_mpileup.sh
@@ -1,21 +1,13 @@
-#!/bin/bash
-#
-#$ -cwd
-#$ -j y
-#$ -S /bin/bash
-#
-
-export PATH=/share/apps/R-3.2.2/bin:$PATH
-
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr2:184936178-184936178 #ZNF804A
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr1:98046571-98046571 #MIR137HG
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr2:192775750-192775750 #PCGEM1
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr6:30186422-30186422 #TRIM26
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr8:2937908-2937908 #CSMD1
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr8:88039069-88039069 #MMP16
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr10:103054405-103054405 #CNNM2
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr10:103089711-103089711 #NT5C2
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr11:125609468-125609468 #STT3A
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr18:54903114-54903114 #CCDC68
-bash ~/LabNotes/SubmissionScripts/mpileup.sh chr18:55228300-55228300 #TCF4
-
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr2:184936178-184936178 #ZNF804A
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr1:98046571-98046571 #MIR137HG
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr2:192775750-192775750 #PCGEM1
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr6:30186422-30186422 #TRIM26
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr8:2937908-2937908 #CSMD1
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr8:88039069-88039069 #MMP16
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr10:102864315-102864315
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr10:102901727-102901727
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr10:103054405-103054405 #CNNM2
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr10:103089711-103089711 #NT5C2
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr11:125609468-125609468 #STT3A
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr18:54903114-54903114 #CCDC68
+qsub ~/LabNotes/SubmissionScripts/mpileup.sh chr18:55228300-55228300 #TCF4
--- SubmissionScripts/mpileup.sh
+++ SubmissionScripts/mpileup.sh
@@ -6,19 +6,42 @@
 #
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
-
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
+for input in `find /c8000xd3/rnaseq-heath/Mappings/ -name *.chr.nonref.merged.sorted.bam`
+do
+    filename1=${input##*/}
+    file_location=${input%/*} 
+    sampleID=${filename1%%.*}
+    echo "Started processing raw mapping of $sampleID"
+
+    samtools mpileup -d 8000 -r $1 \
+    -f /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
+    $input |python ~/LabNotes/Python/CountBases.py $sampleID >> /c8000xd3/rnaseq-heath/Counts/$1.counts.txt
+    echo "Finished processing raw mapping of $sampleID"
+done
+
+for input in `find /c8000xd3/rnaseq-heath/Mappings/ -name *.chr.nonref.merged.dedup.sort.bam`
+do
+    filename1=${input##*/}
+    file_location=${input%/*} 
+    sampleID=${filename1%%.*}
+    echo "Started processing deduplicated mapping of $sampleID"
+
+    samtools mpileup -d 8000 -r $1 \
+    -f /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
+    $input |python ~/LabNotes/Python/CountBases.py $sampleID >> /c8000xd3/rnaseq-heath/Counts/$1.dedup.counts.txt
+    echo "Finished processing deduplicated mapping of $sampleID"
+done
+
 for input in `find /c8000xd3/rnaseq-heath/Mappings/ -name *.chr.nonref.merged.dedup.sort.clip.bam`
 do
     filename1=${input##*/}
     file_location=${input%/*} 
     sampleID=${filename1%%.*}
-    echo "Started processing $sampleID"
-
-
+    echo "Started processing clipped mapping of $sampleID"
 
     samtools mpileup -d 8000 -r $1 \
     -f /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
     $input |python ~/LabNotes/Python/CountBases.py $sampleID >> /c8000xd3/rnaseq-heath/Counts/$1.clip.counts.txt
-    echo "Finished processing $sampleID"
+    echo "Finished processing clipped mapping of $sampleID"
 done
