2eebe4c620bb5a099c44700e93892e4b04139600
Heath O'Brien <heath.obrien@gmail.com>
2016-12-01 14:03:28 +0000

    try coordinate sorting the unmapped file rather than query name sort
    I modified SortSam so that I can specify the sort order (unsorted,
    queryname, coordinate, duplicate)

--- SubmissionScripts/SortSam.sh
+++ SubmissionScripts/SortSam.sh
@@ -5,9 +5,12 @@
 #$ -S /bin/bash
 #
 
+# Usage: qsub SortSam.sh FILENAME SORT_ORDER
+# possible values for SORT_ORDER: unsorted, queryname, coordinate, duplicate
+
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 java -jar ~/src/picard-tools-1.139/picard.jar SortSam \
   I=$1 \
-  O=${1%.*}_qsort.bam \
-  SORT_ORDER=queryname
+  O=${1%.*}_$2.bam \
+  SORT_ORDER=$2
