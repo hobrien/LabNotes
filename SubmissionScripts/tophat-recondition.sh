6c373dadb1606a33a68ff560bfc713f089c3ddf6
hobrien <heath.obrien@gmail.com>
2016-12-01 16:25:37 +0000

    added -q flag to suppress millions of lines of logging
    I also deleted some unnecessary leftovers from a previous script

--- SubmissionScripts/tophat-recondition.sh
+++ SubmissionScripts/tophat-recondition.sh
@@ -7,9 +7,4 @@
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
-infile=$1
-outfile=${1%.*}_RG.bam
-SampleID=$2
-rgid=`find /c8000xd3/databank/foetal-rna/ -name $(grep $SampleID ~/LabNotes/sequences.txt | head -1 | cut -f 1)* | xargs zcat | head -1 | cut -d: -f 3,4,10 | perl -pe 's/:/./g'`
-
-tophat-recondition $1
+python ~/src/tophat-recondition/tophat-recondition.py -q $1
