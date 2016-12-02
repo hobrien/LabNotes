ffa688e3510e34e0d9d51be9366b499ac06c16e0
Heath O'Brien <heath.obrien@gmail.com>
2016-12-02 09:47:48 +0000

    added script to run samtool merge

---
+++ SubmissionScripts/SamtoolsSort.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+# Usage: qsub SortSam.sh FILENAME SORT_ORDER
+# possible values for SORT_ORDER: unsorted, queryname, coordinate, duplicate
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+samtools merge ${1%.*}_merge.bam$1 $2
