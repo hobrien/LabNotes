e28067065fc46bcfb386e81ded72b55d00904cb4
Heath O'Brien <heath.obrien@gmail.com>
2016-12-20 12:52:15 +0000

    typo

--- SubmissionScripts/checkVCF.sh
+++ SubmissionScripts/checkVCF.sh
@@ -5,12 +5,10 @@
 #$ -S /bin/bash
 #
 
-# Usage: qsub SortSam.sh FILENAME SORT_ORDER
-# possible values for SORT_ORDER: unsorted, queryname, coordinate, duplicate
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 python ~/src/checkVCF-20140116/checkVCF.py \
   #-r /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
-  -r ~/src/checkVCF-20140116/hs37d5.fa
+  -r ~/src/checkVCF-20140116/hs37d5.fa \
   -o ${1%.*} $1
