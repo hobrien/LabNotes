401ee0ed7a198dd7a7b07b820c1e635b145f89d7
hobrien <heath.obrien@gmail.com>
2016-12-21 12:49:59 +0000

    increased memory

--- SubmissionScripts/ValidateSAM.sh
+++ SubmissionScripts/ValidateSAM.sh
@@ -7,4 +7,4 @@
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
-java -Xmx2g -jar ~/src/picard-tools-1.139/picard.jar ValidateSamFile I=$1 MODE=SUMMARY
+java -Xmx12g -jar ~/src/picard-tools-1.139/picard.jar ValidateSamFile I=$1 #MODE=SUMMARY
