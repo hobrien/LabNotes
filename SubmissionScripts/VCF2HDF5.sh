5403e7f80cbe1fda195c42eaa75332f6696e7196
hobrien <heath.obrien@gmail.com>
2016-12-21 12:49:32 +0000

    fixed typos and added path info for VCF files

--- SubmissionScripts/VCF2HDF5.sh
+++ SubmissionScripts/VCF2HDF5.sh
@@ -8,9 +8,9 @@
 cd /c8000xd3/rnaseq-heath/Genotypes/Imputation2/
 
 ~/src/WASP-0.2.1/snp2h5/snp2h5 \
-  -- chrom /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/chromInfo.txt
+  --chrom /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/chromInfo.txt \
   --format vcf \
   --haplotype haplotypes.h5 \
   --snp_index snp_index.h5 \
-  --snp_tab   snp_tab.h5 \
-  chr*.GRCh38.sort.vcf.gz
+  --snp_tab snp_tab.h5 \
+  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/Sorted/chr*.GRCh38.sort.vcf.gz
