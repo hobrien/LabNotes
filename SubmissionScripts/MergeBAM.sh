adcc398112f4ea7e6e3d12bf7dd05195dee78173
hobrien <heath.obrien@gmail.com>
2016-12-21 12:39:57 +0000

    changed outfile name

--- SubmissionScripts/MergeBAM.sh
+++ SubmissionScripts/MergeBAM.sh
@@ -8,7 +8,8 @@
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 java -Xmx2g -jar ~/src/picard-tools-1.139/picard.jar MergeBamAlignment \
-       ALIGNED=$1 \ 
-       UNMAPPED=$2 \ 
-       O=${1%.*}.withUnmapped.bam \
+       ALIGNED=$1 \
+       UNMAPPED=$2 \
+       O=${1%.*}_mergeUnmapped.bam \
        R=/c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa
+
