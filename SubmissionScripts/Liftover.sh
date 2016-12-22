b880283bcf84b3dd86893723f059792c3b20755f
hobrien <heath.obrien@gmail.com>
2016-12-22 15:49:32 +0000

    fix recoding, delete bgzip
    - The recoding (adding 'chr') was only working for the variant lines, but not the header
    - Now it recodes the contig line in the header as well
    
    - It looks like CrossMap compresses the file if it's given a '.gz' ending
    - However, this compression isn't working with checkVCF, so I need to uncompress it anyway

--- SubmissionScripts/Liftover.sh
+++ SubmissionScripts/Liftover.sh
@@ -12,13 +12,12 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 #sampleID=${path##*/}
 chr=$1
 bcftools view /c8000xd3/rnaseq-heath/Genotypes/Imputation3/hg19/chr$chr.dose.rename.filter_samples.filter_sites.rsID.vcf.gz \
-  |perl -pe 's/^(\d+)/chr$1/' \
+  |perl -pe 's/^(##contig=<ID=)?(\d+)/$1chr$2/' \
   | bgzip \
   > /c8000xd3/rnaseq-heath/Genotypes/Imputation3/hg19/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.vcf.gz
 CrossMap.py vcf /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/hg19ToHg38.over.chain \
   /c8000xd3/rnaseq-heath/Genotypes/Imputation3/hg19/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.vcf.gz \
   /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
   /c8000xd3/rnaseq-heath/Genotypes/Imputation3/GRCh38/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.vcf
-bgzip /c8000xd3/rnaseq-heath/Genotypes/Imputation3/GRCh38/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.vcf
-rm /c8000xd3/rnaseq-heath/Genotypes/Imputation3/GRCh38/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.vcf
+bcftools index /c8000xd3/rnaseq-heath/Genotypes/Imputation3/GRCh38/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.vcf.gz
 exit $?
