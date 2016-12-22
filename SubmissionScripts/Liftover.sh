cbd057fc9d5fc44127b0cf97db00a5c5ffe60aef
Heath O'Brien <heath.obrien@gmail.com>
2016-12-22 10:28:23 +0000

    fix chromosome number passing
    this was a copy-paste error, where file names from ProcessVCF.sh asked
    for a parameter called $chr, but this script used $1. Now both ask for
    $chr

--- SubmissionScripts/Liftover.sh
+++ SubmissionScripts/Liftover.sh
@@ -10,8 +10,9 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 #path=${1%/*}
 #sampleID=${path##*/}
+chr=$1
 bcftools view /c8000xd3/rnaseq-heath/Genotypes/Imputation3/hg19/chr$chr.dose.rename.filter_samples.filter_sites.rsID.vcf.gz \
-  |perl -pe 's/^(\d+)/chr$1/' \
+  |perl -pe 's/^(\d+)/chr$chr/' \
   | bgzip \
   > /c8000xd3/rnaseq-heath/Genotypes/Imputation3/hg19/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.vcf.gz
 CrossMap.py vcf /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/hg19ToHg38.over.chain \
