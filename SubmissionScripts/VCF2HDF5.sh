469ba9f2f562f44cbb04c7e9def112e33761a732
hobrien <heath.obrien@gmail.com>
2017-01-02 17:36:05 +0000

    moved the filename specification to command line
    - this makes the script more flexiable because it can work with multiple inputs
    - but more importantly, the shell expansion wasn't working within the script
    - it does when it's passed to qsub

--- SubmissionScripts/VCF2HDF5.sh
+++ SubmissionScripts/VCF2HDF5.sh
@@ -13,5 +13,4 @@ cd /c8000xd3/rnaseq-heath/Genotypes/Imputation3/
   --haplotype haplotypes.h5 \
   --snp_index snp_index.h5 \
   --snp_tab snp_tab.h5 \
-  /c8000xd3/rnaseq-heath/Genotypes/Imputation3/GRCh38/chr*.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.filter.vcf.sort.vcf.gz
-  
+  $@
