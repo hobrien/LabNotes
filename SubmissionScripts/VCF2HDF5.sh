5600c383778b9b06e641d370a17e277ae498e24e
Heath O'Brien <heath.obrien@gmail.com>
2017-01-02 13:42:39 +0000

    modified to work with new location of output files

--- SubmissionScripts/VCF2HDF5.sh
+++ SubmissionScripts/VCF2HDF5.sh
@@ -5,7 +5,7 @@
 #$ -S /bin/bash
 #
 
-cd /c8000xd3/rnaseq-heath/Genotypes/Imputation2/
+cd /c8000xd3/rnaseq-heath/Genotypes/Imputation3/
 
 ~/src/WASP-0.2.1/snp2h5/snp2h5 \
   --chrom /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/chromInfo.txt \
@@ -13,4 +13,5 @@ cd /c8000xd3/rnaseq-heath/Genotypes/Imputation2/
   --haplotype haplotypes.h5 \
   --snp_index snp_index.h5 \
   --snp_tab snp_tab.h5 \
-  /c8000xd3/rnaseq-heath/Genotypes/Imputation2/Sorted/chr*.GRCh38.sort.vcf.gz
+  /c8000xd3/rnaseq-heath/Genotypes/Imputation3/GRCh38/chr*.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.filter.vcf.sort.vcf.gz
+  
