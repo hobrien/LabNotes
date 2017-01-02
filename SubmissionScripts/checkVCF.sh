00e95b8d12b3a6fbb66fee7e152cc943bdc20edc
hobrien <heath.obrien@gmail.com>
2017-01-02 17:38:12 +0000

    modified to work with GRCh38 coordinates

--- SubmissionScripts/checkVCF.sh
+++ SubmissionScripts/checkVCF.sh
@@ -9,6 +9,6 @@
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 python ~/src/checkVCF-20140116/checkVCF.py \
-  #-r /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
-  -r ~/src/checkVCF-20140116/hs37d5.fa \
+  -r /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
   -o ${1%.*} $1
+ # -r ~/src/checkVCF-20140116/hs37d5.fa \
