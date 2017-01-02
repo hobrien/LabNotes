939af30a43a10b89e5282609d2daa5128784868c
Heath O'Brien <heath.obrien@gmail.com>
2017-01-02 14:32:11 +0000

    use supplied argument in find command

--- SubmissionScripts/RenameFiles.sh
+++ SubmissionScripts/RenameFiles.sh
@@ -10,7 +10,7 @@ export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 nameStem=$1
 #mv $nameStem $nameStem-1
-for filename in `find 12546-1 -name $nameStem.*`
+for filename in `find $nameStem-1 -name $nameStem.*`
 do 
     echo ${filename#$nameStem}
 done
