3e1babaf5e67e007f526480ca3c5b5a2c72302dc
Heath O'Brien <heath.obrien@gmail.com>
2017-01-02 14:28:11 +0000

    fixed outfile naming

--- SubmissionScripts/SortVCF.sh
+++ SubmissionScripts/SortVCF.sh
@@ -7,4 +7,4 @@
 
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
-/share/apps/vcftools-0.1.14/bin/vcf-sort $1 | bgzip > ${1%.*}.sort.vcf.gz
+/share/apps/vcftools-0.1.14/bin/vcf-sort $1 | bgzip > ${1%.vcf*}.sort.vcf.gz
