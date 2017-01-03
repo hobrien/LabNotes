4788708d7f169f8cca8c99a3c4fe5f99666ff871
Heath O'Brien <heath.obrien@gmail.com>
2017-01-03 10:06:50 +0000

    add location of new sequencing data to find command

--- SubmissionScripts/AddRG.sh
+++ SubmissionScripts/AddRG.sh
@@ -15,11 +15,11 @@ then
     echo "incorrect number of rows for $SampleID"
     exit 0
 fi
-rgid=`find /c8000xd3/databank/foetal-rna/ -name $(grep -P "\s$SampleID(\s|$)"  ~/LabNotes/sequences.txt | head -1 | cut -f 1)*fastq.gz | xargs zcat | head -1 | cut -d: -f 3,4,10 | perl -pe 's/:/./g'`
+rgid=`find /c8000xd2/foetalRNAseq/ /c8000xd3/databank/foetal-rna/ -name $(grep -P "\s$SampleID(\s|$)"  ~/LabNotes/sequences.txt | head -1 | cut -f 1)*fastq.gz | xargs zcat | head -1 | cut -d: -f 3,4,10 | perl -pe 's/:/./g'`
 if [ ! $rgid ]
 then
     echo "trying without gzip"
-    rgid=`find /c8000xd3/databank/foetal-rna/ -name $(grep -P "\s$SampleID(\s|$)"  ~/LabNotes/sequences.txt | head -1 | cut -f 1)*fastq | xargs head -1 | cut -d: -f 3,4,10 | perl -pe 's/:/./g'`
+    rgid=`find /c8000xd2/foetalRNAseq/ /c8000xd3/databank/foetal-rna/ -name $(grep -P "\s$SampleID(\s|$)"  ~/LabNotes/sequences.txt | head -1 | cut -f 1)*fastq | xargs head -1 | cut -d: -f 3,4,10 | perl -pe 's/:/./g'`
 fi
 if [ ! $rgid ]
 then
