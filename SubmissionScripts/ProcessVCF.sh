ae35e9183a962f2ba1c7e5c0ac632bd9f7a2bd5a
Heath O'Brien <heath.obrien@gmail.com>
2017-01-11 12:50:35 +0000

    only run on outputs that don't exist, only run on specified chromosome

--- SubmissionScripts/ProcessVCF.sh
+++ SubmissionScripts/ProcessVCF.sh
@@ -7,8 +7,9 @@
 
 BASEDIR=/c8000xd3/rnaseq-heath/Genotypes/Imputation3
 ls /c8000xd3/rnaseq-heath/Mappings | cut -d- -f 1 | sort | uniq > ~/LabNotes/mappings.txt
-for chr in {1..22}
-do 
+chr = $1
+if [ ! $BASEDIR/hg19/chr$chr.dose.rename.vcf.gz ]
+then 
     echo "Modifying header of chromosome $chr"
     head -13 ~/LabNotes/header.txt > header_temp.txt
     echo "##contig=<ID=$chr>" >> header_temp.txt 
@@ -23,6 +24,9 @@ do
         echo "Could not modify header of chromosome $chr"
         exit 1
     fi
+    rm header_temp.txt
+fi
+if [ ! $BASEDIR/hg19/chr$chr.dose.rename.filter_samples.vcf.gz ]
     echo "Removing unsequenced samples from chromosome $chr"
     # the pysam script that I'm using appears to require a bcf file
     # all subsequent files will be gzipped vcf files, because they take up about half of the space
@@ -40,6 +44,8 @@ do
         rm header_temp.txt
         exit 1
     fi
+fi
+if [ ! $BASEDIR/hg19/chr$chr.dose.rename.filter_samples.filter_sites.vcf.gz ]
     echo "Filtering SNPs with alt allele probabilities < 0.9 for $chr"
     python ~/LabNotes/Python/FilterVCF.py $BASEDIR/hg19/chr$chr.dose.rename.filter_samples.vcf.gz \
       | bcftools view -Oz -o $BASEDIR/hg19/chr$chr.dose.rename.filter_samples.filter_sites.vcf.gz
@@ -50,6 +56,8 @@ do
         echo "Could not filter SNPs for chromosome $chr"
         exit 1
     fi
+fi
+if [ ! $BASEDIR/hg19/chr$chr.dose.rename.filter_samples.filter_sites.rsID.vcf.gz ]
     echo "Adding rsIDs to $chr"
     bcftools index /c8000xd3/rnaseq-heath/Genotypes/Imputation3/hg19/chr$chr.dose.rename.filter_samples.filter_sites.vcf.gz
     bcftools annotate -c ID -Oz \
@@ -64,6 +72,8 @@ do
         echo "Could not add rsIDs to chromosome $chr"
         exit 1
     fi
+fi
+if [ ! $BASEDIR/GRCh38/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.sort.filter.vcf.gz ]
     echo "Liftover of chromosome $chr from hg19 to GRCh38"
     bash ~/LabNotes/SubmissionScripts/Liftover.sh $chr
     if [ $? -eq 0 ]
@@ -73,7 +83,4 @@ do
         echo "Could not do liftover for chromosome $chr"
         exit 1
     fi
-    python ~/LabNotes/Python/FilterVCF.py $BASEDIR/hg19/chr$chr.dose.rename.filter_samples.vcf.gz \
-      | bcftools view -Ob -o $BASEDIR/hg19/chr$chr.dose.rename.filter_samples.filter_sites.vcf.gz
-    rm header_temp.txt
-done
+fi
