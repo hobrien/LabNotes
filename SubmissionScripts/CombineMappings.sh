9a3284cfb43f539292b217b98f0c470edc6abb4e
hobrien <heath.obrien@gmail.com>
2017-01-11 10:14:49 +0000

    use Picard to merg Bam files Samtools merge does not merge the read group info in the header

--- SubmissionScripts/CombineMappings.sh
+++ SubmissionScripts/CombineMappings.sh
@@ -10,9 +10,8 @@ BASEDIR=/c8000xd3/rnaseq-heath/Mappings/
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 SampleID=$1
 #bash ~/LabNotes/SubmissionScripts/RenameFiles $SampleID
-#mkdir -p $BASEDIR/$SampleID/BAM
-#Rscript ~/LabNotes/R/CombineCounts.R $SampleID
+mkdir -p $BASEDIR/$SampleID/BAM
+Rscript ~/LabNotes/R/CombineCounts.R $SampleID
 
-bash ~/LabNotes/SubmissionScripts/SamtoolsMerge.sh `find $BASEDIR -name accepted_hits_fixup_merge_sort.bam | grep $SampleID- | sort`
-mv $BASEDIR/$SampleID-1/BAM/accepted_hits_fixup_merge_sort_merge.bam $BASEDIR/$SampleID/BAM/
-	
+bash ~/LabNotes/SubmissionScripts/MergeSAM.sh `find $BASEDIR -name accepted_hits_filtered_sort_dedup_sort_RG.bam | grep $SampleID- | sort`
+mv $BASEDIR/$SampleID-1/BAM/accepted_hits_filtered_sort_dedup_sort_RG_merge.bam $BASEDIR/$SampleID/BAM/$SampleID.bam
