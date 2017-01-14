cbffc98901e44be67e7982e58928f0999fc694b5
Heath O'Brien <heath.obrien@gmail.com>
2017-01-14 17:47:32 +0000

    Run ASEReadCounter on all chromosomes

--- SubmissionScripts/ASEReadCounter.sh
+++ SubmissionScripts/ASEReadCounter.sh
@@ -5,19 +5,18 @@
 #$ -S /bin/bash
 #
 
-export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
-
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 filename=${1##*/}
 sampleID=${filename%%_*} #This will remove the library ID, lane number and read number
-outfilename=/c8000xd3/rnaseq-heath/Counts/$sampleID.ase.rtable
+chr=$2
 
 java -jar /share/apps/GenomeAnalysisTK.jar -T ASEReadCounter \
    -R /c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa \
-   -o ${1%.*}.22.ase.rtable \
+   -o ${1%.*}.$chr.ase.rtable \
    -I $1 \
-   -sites /c8000xd3/rnaseq-heath/Genotypes/Imputation3/GRCh38/chr22.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.sort.filter.vcf.gz \
+   -sites /c8000xd3/rnaseq-heath/Genotypes/Imputation3/GRCh38/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.sort.filter.vcf.gz \
    -U ALLOW_N_CIGAR_READS \
    -minDepth 10 \
    --minMappingQuality 10 \
    --minBaseQuality 2
+exit $?
--- SubmissionScripts/CombineMappings.sh
+++ SubmissionScripts/CombineMappings.sh
@@ -10,9 +10,88 @@ BASEDIR=/c8000xd3/rnaseq-heath/Mappings/
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
 SampleID=$1
 #bash ~/LabNotes/SubmissionScripts/RenameFiles $SampleID
-mkdir -p $BASEDIR/$SampleID/BAM
-Rscript ~/LabNotes/R/CombineCounts.R $SampleID
+echo "Combining mappings for $SampleID"
+if [ ! -d $BASEDIR/$SampleID/BAM ]
+then
+    echo "Making directory $BASEDIR/$SampleID/BAM"
+    mkdir -p $BASEDIR/$SampleID/BAM
+    if [ $? -eq 0 ]
+    then
+        echo "Finished making directory $BASEDIR/$SampleID/BAM"
+    else
+        echo "Could make directory $BASEDIR/$SampleID/BAM"
+        exit 1
+    fi
+fi
+if [ ! -f $BASEDIR/$SampleID/BAM/$SampleID.chr.counts.txt ]
+then
+    echo "Combining counts for $SampleID"
+    Rscript ~/LabNotes/R/CombineCounts.R $SampleID
+    if [ $? -eq 0 ]
+    then
+        echo "Finished combining counts for $SampleID"
+    else
+        echo "Could combine counts for $SampleID"
+        exit 1
+    fi
+fi
 
-bash ~/LabNotes/SubmissionScripts/MergeSAM.sh `find $BASEDIR -name accepted_hits_filtered_sort_dedup_sort_RG.bam | grep $SampleID- | sort`
-mv $BASEDIR/$SampleID-1/BAM/accepted_hits_filtered_sort_dedup_sort_RG_merge.bam $BASEDIR/$SampleID/BAM/$SampleID.bam
-bash ~/LabNotes/SubmissionScripts/SamtoolsIndex.sh $BASEDIR/$SampleID/BAM/$SampleID.bam
+if [ ! -f $BASEDIR/$SampleID/BAM/$SampleID.bam ]
+then
+    echo "Merging mappings for $SampleID"
+    bash ~/LabNotes/SubmissionScripts/MergeSAM.sh `find $BASEDIR -name accepted_hits_filtered_sort_dedup_sort_RG.bam | grep $SampleID- | sort`
+    mv $BASEDIR/$SampleID-1/BAM/accepted_hits_filtered_sort_dedup_sort_RG_merge.bam $BASEDIR/$SampleID/BAM/$SampleID.bam
+
+    if [ $? -eq 0 ]
+    then
+        echo "Finished merging mappings for $SampleID"
+    else
+        echo "Could merge mappings for $SampleID"
+        exit 1
+    fi
+fi
+
+if [ ! -f $BASEDIR/$SampleID/BAM/$SampleID.bam.bai ]
+then
+    echo "Indexing merged mappings for $SampleID"
+    bash ~/LabNotes/SubmissionScripts/SamtoolsIndex.sh $BASEDIR/$SampleID/BAM/$SampleID.bam
+
+    if [ $? -eq 0 ]
+    then
+        echo "Finished indexing merged mappings for $SampleID"
+    else
+        echo "Could index merged mappings for $SampleID"
+        exit 1
+    fi
+fi
+
+for chr in {1..22}
+do
+    if [ ! -f /c8000xd3/rnaseq-heath/Genotypes/Imputation3/GRCh38/chr$chr.dose.rename.filter_samples.filter_sites.rsID.recoded.GRCh38.sort.filter_nonSNP.vcf.gz ]
+    then
+        echo "Running ProcessVCF on $chr"
+        bash ~/LabNotes/SubmissionScripts/ProcessVCF.sh $chr
+        if [ $? -eq 0 ]
+        then
+            echo "Finished running ProcessVCF on $chr"
+        else
+            echo "Could not run ProcessVCF on $chr"
+            exit 1
+        fi
+    fi
+    if [ ! -f if [ ! -f $BASEDIR/$SampleID/BAM/$SampleID.$chr.ase.rtable ]
+    then
+        echo "Running ASEReadCounter on $chr for $SampleID"
+        bash ~/LabNotes/SubmissionScripts/ASEReadCounter.sh $BASEDIR/$SampleID/BAM/$SampleID.bam $chr
+        if [ $? -eq 0 ]
+        then
+            echo "Finished running ASEReadCounter on $chr for $SampleID"
+        else
+            echo "Could not run ASEReadCounter on $chr for $SampleID"
+            exit 1
+        fi
+    fi
+done
+echo "Finished combining mappings for $SampleID"
+exit $?
+
