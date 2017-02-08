8e137c5394fe618a5e442a5be6202d6bcf8b6f33
hobrien <heath.obrien@gmail.com>
2017-02-08 18:04:00 +0000

    removed duplicated line

--- SubmissionScripts/CombineCounts.sh
+++ SubmissionScripts/CombineCounts.sh
@@ -1,4 +1,4 @@
 for SampleID in 11875 12107 12545 12546 12993 12994 13008 13142 15533 16483 17046 17054 17221 17264 18153 18540 18655 A138  A226 
 do
-    grep $SampleID ~/LabNotes/chr_bam_R1.txt | cut -f 1,2 | perl -pe 's/\s+/BAM\//' | perl -pe 's/bam/counts.txt/' | sort | uniq | xargs Rscript R/CombineCounts.R chr_bam_R1.txt | cut -f 1,2 | perl -pe 's/\s+/BAM\//' | perl -pe 's/bam/counts.txt/' | sort | uniq | xargs Rscript ~/LabNotes/R/CombineCounts.R
+    grep $SampleID ~/LabNotes/chr_bam_R1.txt | cut -f 1,2 | perl -pe 's/\s+/BAM\//' | perl -pe 's/bam/counts.txt/' | sort | uniq | xargs Rscript ~/LabNotes/R/CombineCounts.R 
 done
