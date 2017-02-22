78ab13473eb9d45a41e94be6483ba2e8098de94e
hobrien <heath.obrien@gmail.com>
2017-02-22 14:17:56 +0000

    added error checking to see if download completes

--- SubmissionScripts/Curl.sh
+++ SubmissionScripts/Curl.sh
@@ -3,4 +3,15 @@
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
-curl -O -L "$@" 
+for file in $@
+do
+    echo "Downloading $file"
+    curl -O -L "$file"
+    if [ $? -eq 0 ]
+    then
+        echo "Finished downloading $file"
+    else
+        echo "Could not download $file"
+        exit 1
+    fi    
+done 
