3259bb24c52f1b91a1654857e7641ad24729df57
hobrien <heath.obrien@gmail.com>
2017-06-05 18:50:45 +0100

    Submit each step as a separate job, rather than running as bash commands
    This allows different resources to be called for each step rather than
    requiring 8 nodes for all steps including ones that are not parallelised.
    
    This makes use of named jobs and the -hold_jid qsub argument.
    
    I've commented out all of the logging of each step because thes qsub
    commands complete immediately rather than waiting for the step to complete
    as they did when they were bash commands.
    
    I've also added comments to indicate the CPU and Wallclock times for each
    step becasuse there appear to be some that are using more resources than
    I'm asking for

--- SubmissionScripts/MappingPipeline.sh
+++ SubmissionScripts/MappingPipeline.sh
@@ -3,7 +3,6 @@
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
-#$ -pe smp 8
 #
 
 # This will take an individual pair of read files and take them through all steps of
@@ -19,6 +18,7 @@ MAPPER=HISAT2
 BASEDIR=/c8000xd3/rnaseq-heath/ASEmappings
 ANNOTATION=/c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Annotation/Genes.gencode
 REF=/c8000xd3/rnaseq-heath/Ref/Homo_sapiens/GRCh38/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta
+sequences=$(for name in `grep -P "\s$SampleID(\s|$)"  ~/LabNotes/sequences.txt | cut -f 1`; do find /neurocluster/sequencers/foetalRNAseq/ /neurocluster/databank/foetal-rna/ -name $name*f*q.gz; done)
 
 echo "Starting mapping for $BASEDIR/$SampleID"
 if [ ! -d $BASEDIR/$SampleID ]
@@ -37,7 +37,6 @@ fi
 if [ ! -f $BASEDIR/$SampleID/$SampleID.bam ]
 then
     echo "$MAPPER mapping for $SampleID"
-    sequences=$(for name in `grep -P "\s$SampleID(\s|$)"  ~/LabNotes/sequences.txt | cut -f 1`; do find /c8000xd2/foetalRNAseq/ /c8000xd3/databank/foetal-rna/ -name $name*f*q.gz; done)
     if [ ! $sequences ]
     then
         echo "trying without gzip"
@@ -51,227 +50,232 @@ then
     echo "Read files: $sequences"
     sequences="$sequences $BASEDIR/$SampleID/$SampleID.bam" # Add output file to arguments
     qsub -N h${SampleID}_map ~/LabNotes/SubmissionScripts/HISAT2.sh $sequences
-    if [ $? -eq 0 ]
-    then
-        echo "Finished $MAPPER mapping for $SampleID"
-    else
-        echo "$MAPPER mapping failed on $SampleID"
-        rm $BASEDIR/$SampleID/$SampleID.bam
-        exit 1
-    fi    
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished $MAPPER mapping for $SampleID"
+#     else
+#         echo "$MAPPER mapping failed on $SampleID"
+#         rm $BASEDIR/$SampleID/$SampleID.bam
+#         exit 1
+#     fi    
 fi
 
 if [ ! -f $BASEDIR/$SampleID/${SampleID}_sort.bam ]
 then
     echo "Sorting $SampleID"
-    samtools sort -o $BASEDIR/$SampleID/${SampleID}_sort.bam $BASEDIR/$SampleID/$SampleID.bam
-    if [ $? -eq 0 ]
-    then
-        echo "Finished sorting for $SampleID"
-    else
-        echo "Could not sort BAM for $SampleID"
-        exit 1
-    fi
+    qsub -N h${SampleID}_sort1 -hold_jid h${SampleID}_map \
+      ~/LabNotes/SubmissionScripts/SamtoolsSort.sh \
+      $BASEDIR/$SampleID/$SampleID.bam
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished sorting for $SampleID"
+#     else
+#         echo "Could not sort BAM for $SampleID"
+#         exit 1
+#     fi
 fi   
 
 if [ ! -f $BASEDIR/$SampleID/${SampleID}_sort.bam.bai ]
 then
     echo "Indexing $SampleID"
-    samtools index $BASEDIR/$SampleID/${SampleID}_sort.bam    
-    if [ $? -eq 0 ]
-    then
-        echo "Finished indexing for $SampleID"
-    else
-        echo "Could not index BAM for $SampleID"
-        exit 1
-    fi
+    qsub -N h${SampleID}_index1 -hold_jid h${SampleID}_sort1 \
+      ~/LabNotes/SubmissionScripts/SamtoolsIndex.sh \
+      $BASEDIR/$SampleID/${SampleID}_sort.bam    
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished indexing for $SampleID"
+#     else
+#         echo "Could not index BAM for $SampleID"
+#         exit 1
+#     fi
 fi
 
-if [ ! -f $BASEDIR/$SampleID/${SampleID}_stats.txt ]
+# 1118.769 CPU / 1121 Wallclock = 1
+if [ ! -f $BASEDIR/$SampleID/${SampleID}_sort_stats.txt ]
 then
     echo "Running RNAseqQC $SampleID"
-    bam_stat.py -i $BASEDIR/$SampleID/${SampleID}_sort.bam > $BASEDIR/$SampleID/${SampleID}_stats.txt
-    if [ $? -eq 0 ]
-    then
-        echo "Finished running RNAseqQC for $SampleID"
-    else
-        echo "Could not run RNAseqQC for $SampleID"
-        exit 1
-    fi
+    qsub -N h${SampleID}_stats -hold_jid h${SampleID}_index1 \
+      ~/LabNotes/SubmissionScripts/GetStats.sh \
+      $BASEDIR/$SampleID/${SampleID}_sort.bam
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished running RNAseqQC for $SampleID"
+#     else
+#         echo "Could not run RNAseqQC for $SampleID"
+#         exit 1
+#     fi
 fi
 
+# 77140.787 CPU / 77475 Wallclock
 if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_sort.remap.fq1.gz ] || [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_sort.remap.fq2.gz ]
 then
     echo "finding intersecting SNPs for $SampleID"
     mkdir $BASEDIR/$SampleID/find_intersecting_snps
-    python ~/src/WASP-0.2.1/mapping/find_intersecting_snps.py \
-          --is_paired_end \
-          --is_sorted \
-          --output_dir $BASEDIR/$SampleID/find_intersecting_snps/ \
-          --snp_tab /c8000xd3/rnaseq-heath/Genotypes/Imputation3/HDF5/snp_tab.h5 \
-          --snp_index /c8000xd3/rnaseq-heath/Genotypes/Imputation3/HDF5/snp_index.h5 \
-          --haplotype /c8000xd3/rnaseq-heath/Genotypes/Imputation3/HDF5/haplotypes.h5 \
-          $BASEDIR/$SampleID/${SampleID}_sort.bam
-    if [ $? -eq 0 ]
-    then
-        echo "Finished finding intersecting SNPs for $SampleID"
-    else
-        echo "Could not find intersecting SNPs $SampleID"
-        exit 1
-    fi  
+    qsub -N h${SampleID}_wasp1 -hold_jid h${SampleID}_index1 \
+      ~/LabNotes/SubmissionScripts/FindIntersectingSNPS.sh \
+      $BASEDIR/$SampleID/${SampleID}_sort.bam
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished finding intersecting SNPs for $SampleID"
+#     else
+#         echo "Could not find intersecting SNPs $SampleID"
+#         exit 1
+#     fi  
 fi  
 
+#29495.830 CPU / 3635 Wallclock = 8.11
 if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap.bam ]
 then
     echo "remapping reads with intersecting SNPs for $SampleID"
-    bash ~/LabNotes/SubmissionScripts/HISAT2.sh \
+    qsub -N h${SampleID}_remap -hold_jid h${SampleID}_wasp1 ~/LabNotes/SubmissionScripts/HISAT2.sh \
       $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_sort.remap.fq1.gz \
       $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_sort.remap.fq2.gz \
-      | samtools view -S -bo $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap.bam -
+      $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap.bam
 
-    if [ $? -eq 0 ]
-    then
-        echo "Finished remapping reads for $SampleID"
-    else
-        echo "Could not remap reads for $SampleID"
-        exit 1
-    fi
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished remapping reads for $SampleID"
+#     else
+#         echo "Could not remap reads for $SampleID"
+#         exit 1
+#     fi
 fi
      
 if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort.bam ]
 then
     echo "Sorting remapped BAM $SampleID"
-    samtools sort -o $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort.bam \
+    qsub -N h${SampleID}_sort2 -hold_jid h${SampleID}_remap \
+      ~/LabNotes/SubmissionScripts/SamtoolsSort.sh \
       $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap.bam
       
-    if [ $? -eq 0 ]
-    then
-        echo "Finished sorting remapped BAM for $SampleID"
-    else
-        echo "Could not sort remapped BAM for $SampleID"
-        exit 1
-    fi
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished sorting remapped BAM for $SampleID"
+#     else
+#         echo "Could not sort remapped BAM for $SampleID"
+#         exit 1
+#     fi
 fi   
 
+# 210.741 CPU / 218 Wallclock
 if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort.bam.bai ]
 then
     echo "Indexing remapped BAM for $SampleID"
-    samtools index $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort.bam    
-    if [ $? -eq 0 ]
-    then
-        echo "Finished indexing remapped BAM for $SampleID"
-    else
-        echo "Could not index remapped BAM for $SampleID"
-        exit 1
-    fi
+    qsub -N h${SampleID}_index2 -hold_jid h${SampleID}_sort2 \
+      ~/LabNotes/SubmissionScripts/SamtoolsIndex.sh \
+       $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort.bam    
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished indexing remapped BAM for $SampleID"
+#     else
+#         echo "Could not index remapped BAM for $SampleID"
+#         exit 1
+#     fi
 fi
 
+# 5241.446 CPU / 5805 Wallclock
 if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep.bam ]
 then
     echo "Filtering remapped reads for $SampleID"
-    python ~/src/WASP-0.2.1/mapping/filter_remapped_reads.py \
-          $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_sort.to.remap.bam \
-          $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort.bam \
-          $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep.bam 
-    if [ $? -eq 0 ]
-    then
-        echo "Finished filtering remapped reads for $SampleID"
-    else
-        echo "Could not filter remapped reads for $SampleID"
-        exit 1
-    fi
+    qsub -N h${SampleID}_filter -hold_jid h${SampleID}_index2 \
+      ~/LabNotes/SubmissionScripts/FilterRemappedReads.sh $SampleID
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished filtering remapped reads for $SampleID"
+#     else
+#         echo "Could not filter remapped reads for $SampleID"
+#         exit 1
+#     fi
 fi
 
-if [ ! -f $BASEDIR/$SampleID/${SampleID}_sort_filtered.bam ]
+# 1962.853 CPU / 1964 Wallclock
+if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge.bam ]
 then
     echo "Merging filtered reads for $SampleID"
-    samtools merge $BASEDIR/$SampleID/${SampleID}_sort_filtered.bam \
+    qsub -N h${SampleID}_merge -hold_jid h${SampleID}_filter \
+      ~/LabNotes/SubmissionScripts/SamtoolsMerge.sh \
               $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep.bam  \
               $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_sort.keep.bam
-    if [ $? -eq 0 ]
-    then
-        echo "Finished merging filtered reads for $SampleID"
-    else
-        echo "Could not merge filtered reads for $SampleID"
-        exit 1
-    fi
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished merging filtered reads for $SampleID"
+#     else
+#         echo "Could not merge filtered reads for $SampleID"
+#         exit 1
+#     fi
 fi
 
-if [ ! -f $BASEDIR/$SampleID/${SampleID}_filtered_sort.bam ]
+if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge_sort.bam ]
 then
     echo "Sorting filtered BAM $SampleID"
-    samtools sort -o $BASEDIR/$SampleID/${SampleID}_filtered_sort.bam \
-      $BASEDIR/$SampleID/${SampleID}_sort_filtered.bam 
-    if [ $? -eq 0 ]
-    then
-        echo "Finished sorting filtered BAM for $SampleID"
-    else
-        echo "Could not sort filtered BAM for $SampleID"
-        exit 1
-    fi
+    qsub -N h${SampleID}_sort3 -hold_jid h${SampleID}_merge \
+      ~/LabNotes/SubmissionScripts/SamtoolsSort.sh \
+      $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge.bam 
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished sorting filtered BAM for $SampleID"
+#     else
+#         echo "Could not sort filtered BAM for $SampleID"
+#         exit 1
+#     fi
 fi   
 
-if [ ! -f $BASEDIR/$SampleID/${SampleID}_filtered_sort.bam.bai ]
+if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge_sort.bam.bai ]
 then
     echo "Indexing filtered BAM for $SampleID"
-    samtools index $BASEDIR/$SampleID/${SampleID}_filtered_sort.bam 
-    if [ $? -eq 0 ]
-    then
-        echo "Finished indexing filtered BAM for $SampleID"
-    else
-        echo "Could not index filtered BAM for $SampleID"
-        exit 1
-    fi
+    qsub -N h${SampleID}_index3 -hold_jid h${SampleID}_sort3 \
+      ~/LabNotes/SubmissionScripts/SamtoolsIndex.sh \
+       $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge_sort.bam 
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished indexing filtered BAM for $SampleID"
+#     else
+#         echo "Could not index filtered BAM for $SampleID"
+#         exit 1
+#     fi
 fi
 
-if [ ! -f $BASEDIR/$SampleID/${SampleID}_filtered_sort_dedup.bam ]
+# 3552.248 CPU / 3599 wallclock = 0.99
+if [ ! -f  $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge_sort_dedup.bam ]
 then
     echo "Deduplicating filtered BAM for $SampleID"
-    python ~/src/WASP-0.2.1/mapping/rmdup_pe.py \
-        $BASEDIR/$SampleID/${SampleID}_filtered_sort.bam  \
-        $BASEDIR/$SampleID/${SampleID}_filtered_sort_dedup.bam
-    if [ $? -eq 0 ]
-    then
-        echo "Finished deduplicating filtered BAM for $SampleID"
-    else
-        echo "Could not deduplicate filtered BAM for $SampleID"
-        exit 1
-    fi
+    qsub -N h${SampleID}_dedup -hold_jid h${SampleID}_index3 \
+      ~/LabNotes/SubmissionScripts/Dedup.sh \
+       $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge_sort.bam
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished deduplicating filtered BAM for $SampleID"
+#     else
+#         echo "Could not deduplicate filtered BAM for $SampleID"
+#         exit 1
+#     fi
 fi
 
-if [ ! -f $BASEDIR/$SampleID/${SampleID}_filtered_dedup_sort.bam ]
+#4128.530 CPU / 4195 Wallclock = 0.98
+if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge_sort_dedup_sort.bam ]
 then
     echo "Sorting filtered, deduplicated BAM for $SampleID"
     # Sort BAM files by query name
-    samtools sort -o $BASEDIR/$SampleID/${SampleID}_filtered_dedup_sort.bam \
-      $BASEDIR/$SampleID/${SampleID}_filtered_sort_dedup.bam 
-      
-    if [ $? -eq 0 ]
-    then
-        echo "Finished sortting filtered, deduplicated BAM for $SampleID"
-    else
-        echo "Could not sort filtered, deduplicated BAM for $SampleID"
-        exit 1
-    fi    
+    qsub -N h${SampleID}_sort4 -hold_jid h${SampleID}_dedup \
+    ~/LabNotes/SubmissionScripts/SamtoolsSort.sh \
+    $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge_sort_dedup.bam 
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Finished sorting filtered, deduplicated BAM for $SampleID"
+#     else
+#         echo "Could not sort filtered, deduplicated BAM for $SampleID"
+#         exit 1
+#     fi    
 fi
 
-if [ ! -f $BASEDIR/$SampleID/${SampleID}_filtered_dedup_sort.bam.bai ]
-then
-    echo "Indexing filtered, deduplicated BAM for $SampleID"
-    samtools index $BASEDIR/$SampleID/${SampleID}_filtered_dedup_sort.bam 
-    if [ $? -eq 0 ]
-    then
-        echo "Finished indexing filtered, deduplicated BAM for $SampleID"
-    else
-        echo "Could not index filtered, deduplicated BAM for $SampleID"
-        exit 1
-    fi
-fi
-
-if [ ! -f $BASEDIR/$SampleID/${SampleID}_filtered_dedup_sort_RG.bam ]
+# 1862.636 CPU = 1719 Wallclock
+if [ ! -f $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge_sort_dedup_sort_RG.bam ]
 then
     echo "Adding read groups for $SampleID"
-    bash ~/LabNotes/SubmissionScripts/AddRG.sh $BASEDIR/$SampleID/${SampleID}_filtered_dedup_sort.bam $SampleID
+    qsub -N h${SampleID}_rg -hold_jid h${SampleID}_sort4 \
+      ~/LabNotes/SubmissionScripts/AddRG.sh \
+      $BASEDIR/$SampleID/find_intersecting_snps/${SampleID}_remap_sort_keep_merge_sort_dedup_sort.bam \
+      $SampleID
     if [ $? -eq 0 ]
     then
         echo "Finished adding read groups for $SampleID"
