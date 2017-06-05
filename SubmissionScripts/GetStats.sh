711aa51dd9832f0514c5f815425ccf5cd291360a
hobrien <heath.obrien@gmail.com>
2017-06-05 18:58:31 +0100

    stripped down version of RNAseqQC that just calculates basic stats

---
+++ SubmissionScripts/GetStats.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
+
+export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
+
+# see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
+for input in $@
+#for input in `find /c8000xd3/rnaseq-heath/Mappings/ -name *.chr.nonref.merged.dedup.sort.clip.bam`
+do
+    
+    echo "Started processing $input"
+    bam_stat.py -i $input > ${input%.*}_stats.txt
+    echo "Finished processing $input"
+done
+exit $?
