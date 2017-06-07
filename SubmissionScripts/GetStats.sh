0117da2595ca7ba82c0a862881aa51a34623ab27
Heath O'Brien <heath.obrien@gmail.com>
2017-06-07 13:07:07 +0100

    add explanation of output in comments

--- SubmissionScripts/GetStats.sh
+++ SubmissionScripts/GetStats.sh
@@ -8,6 +8,11 @@
 export PATH=/share/apps/R-3.2.2/bin:/share/apps/:$PATH
 
 # see http://www.tldp.org/LDP/LG/issue18/bash.html for bash Parameter Substitution
+
+# unique + non-unique + unmapped + non-primary = total records
+# unique + non-unique + unmapped = total reads
+# Read-1 + Read-2 = unique
+# Reads map to '+' + Reads map to '-' = unique
 for input in $@
 #for input in `find /c8000xd3/rnaseq-heath/Mappings/ -name *.chr.nonref.merged.dedup.sort.clip.bam`
 do
